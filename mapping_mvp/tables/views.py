32c4555e55347712a8f484faebcd31ee5e35cee3
Simon Tarr <simon.tarr@nottingham.ac.uk>
2021-01-05 14:45:29 +0000

    AB#29427 Added very basic HTML view of database

--- mapping_mvp/tables/views.py
+++ mapping_mvp/tables/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from tables.models import LocalDB, ControlledVocab, OmopMapping
 
-# Create your views here.
+# Simple view to pull all records from the dummy database for display on index.html
+def index(locdb):
+
+    # Pull in all entries in each database (model)
+    loc = LocalDB.objects.all()
+    vocab = ControlledVocab.objects.all()
+    omop = OmopMapping.objects.all()
+
+    # Create quick context dict
+    context = {
+        'loc':loc,
+        'vocab':vocab,
+        'omop':omop
+    }
+
+    return render(locdb, 'tables/index.html', context)
