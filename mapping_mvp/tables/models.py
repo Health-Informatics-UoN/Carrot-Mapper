5e9a05f02b37629846ab77456524330f6e098e14
Simon Tarr <simon.tarr@nottingham.ac.uk>
2021-01-07 16:32:47 +0000

    Added simple 1:many models

--- mapping_mvp/tables/models.py
+++ mapping_mvp/tables/models.py
@@ -1,29 +1,70 @@
 from django.db import models
 
-# Create model which defines the local databases' data
-class LocalDB(models.Model):
-    localDB_name = models.CharField(max_length = 256, unique = False)
-    localDB_table = models.CharField(max_length = 265, unique = False)
-    localDB_column = models.CharField(max_length = 256, unique = False)
-    localDB_value = models.CharField(max_length = 256, unique = False)
-    term = models.CharField(max_length = 256, unique = False)
+# 1:many
+# where a parent record in one table can potentially reference several child records in another table.
+# Therefore 1:many relationships allows zero child records, a single child record or multiple child records.
+#
+# Each table-field combination in the source data can map to multiple locations in Omop
+# e.g. PANTHER:PATIENT:GENDER could map to both PATIENT:GENDER_CONCEPT_ID and OBSERVATION:GENDER_CONCEPT_ID
+
+# For every row in Source, reference Mapping multiple times
+
+# DEFINE MODEL TO HOLD THE POSSIBLE OMOP MAPPING COMBINATIONS
+class Mapping(models.Model):
+    table = models.CharField(max_length=64)
+    field = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'{self.localDB_name, self.localDB_table, self.localDB_value, self.localDB_column, self.term}'
+        return f'{self.table, self.field}'
 
-# Create model which defines the controlled vocabulary list
-class ControlledVocab(models.Model):
-    term = models.CharField(max_length = 256, unique = True)
-
-    def __str__(self):
-        return f'{self.term}'
-
-# Create model which defines the OMOP mapping values
-class OmopMapping(models.Model):
-    controlled_vocab_id = models.ForeignKey(ControlledVocab, on_delete=models.CASCADE)
-    omop_table = models.CharField(max_length = 265, unique = False)
-    omop_column = models.CharField(max_length = 265, unique = False)
-    omop_value = models.CharField(max_length = 265, unique = False)
+# DEFINE MODEL TO HOLD INFORMATION ON THE SOURCE DATA TABLES AND COLUMNS
+class Source(models.Model):
+    dataset = models.CharField(max_length=64)
+    table = models.CharField(max_length=64)
+    field = models.CharField(max_length=64)
+    mapping = models.ManyToManyField(Mapping)
 
     def __str__(self):
-        return f'{self.omop_table, self.omop_column} - {self.omop_value}'
+        return f'{self.dataset, self.table, self.field}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### OLD MODEL CODE FROM RETRO MEETING ON 07/01 ###
+
+# class OmopTable(models.Model):
+#     table_name = models.CharField(max_length=64, unique=True)
+#
+#     def __str__(self):
+#         return f'{self.table_name}'
+#
+# class OmopField(models.Model):
+#     table_name = models.ForeignKey(OmopTable, on_delete=models.CASCADE)
+#     field_name = models.CharField(max_length=64, unique=True)
+#
+#     def __str__(self):
+#         return f'{self.table_name, self.field_name}'
+#
+# class MappingTable(models.Model):
+#     localDB_name = models.CharField(max_length = 256, unique = False)
+#     localDB_table = models.CharField(max_length = 265, unique = False)
+#     localDB_column = models.CharField(max_length = 256, unique = False)
+#     localDB_value = models.CharField(max_length = 256, unique = False)
+#
+#     omop_table = models.ForeignKey(OmopTable, on_delete=models.CASCADE)
+#     omop_field = models.ForeignKey(OmopField, on_delete=models.CASCADE)
+#
+#     comments = models.TextField(blank=True)
+#
+#     def __str__(self):
+#         return f'{self.omop_table, self.omop_field}'
--- mapping_mvp/tables/views.py
+++ mapping_mvp/tables/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from tables.models import LocalDB, ControlledVocab, OmopMapping
+from tables.models import Source, Mapping
 
 # Simple view to pull all records from the dummy database for display on index.html
 def index(locdb):
 
     # Pull in all entries in each database (model)
-    loc = LocalDB.objects.all()
-    vocab = ControlledVocab.objects.all()
-    omop = OmopMapping.objects.all()
+    mapping = Mapping.objects.all()
+    source = Source.objects.all()
 
     # Create quick context dict
     context = {
-        'loc':loc,
-        'vocab':vocab,
-        'omop':omop
+        'source':source,
+        'mapping':mapping,
     }
 
     return render(locdb, 'tables/index.html', context)
