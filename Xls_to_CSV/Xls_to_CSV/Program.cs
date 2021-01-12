1ed4e2655d16a03da310142b28a4787bdfcd654f
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2021-01-12 16:29:01 +0000

    Add .Net project
    Convert xlsx to csv using .net

---
+++ Xls_to_CSV/Xls_to_CSV/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using Aspose.Cells;
+namespace Xls_to_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aspose.Cells.Workbook workbook = new Aspose.Cells.Workbook("/Users/vasilikipanagi/Documents/data/Phenobase1.xlsx");
+            for (int idx = 0; idx < workbook.Worksheets.Count; idx++)
+            {
+               
+                Aspose.Cells.Worksheet worksheet =  workbook.Worksheets[idx];
+                Workbook wkb = new Workbook();
+                wkb.Worksheets.Clear();
+                Worksheet ws = wkb.Worksheets[wkb.Worksheets.Add()];
+                ws.Copy(worksheet);
+                ws.Name = worksheet.Name;
+                var name = worksheet.Name;
+                wkb.Save("/Users/vasilikipanagi/Documents/data/" + name.ToString() + ".csv", Aspose.Cells.SaveFormat.CSV);
+
+
+            }
+
+        }
+    }
+}
+
