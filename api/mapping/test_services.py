ec3de04355aecc57e60607b7d5e724e1c445c2be
Simon Tarr <simon.tarr@nottingham.ac.uk>
2021-01-29 15:39:20 +0000

    Uncommented test

--- api/mapping/test_services.py
+++ api/mapping/test_services.py
@@ -4,15 +4,15 @@ from .services import process_scan_report_sheet_table, run_usagi
 
 class ServiceTests(TestCase):
 
-    # def test_process_scan_report_sheet_table(self):
-    #     # Tell it what file you want to process
-    #     filename = '/api/mapping/data/value_frequency_test_data.csv'
-    #
-    #     # Run the test on filename
-    #     # This function is in services.py
-    #     result = process_scan_report_sheet_table(filename)
-    #
-    #     self.assertIs(4, len(result))
+    def test_process_scan_report_sheet_table(self):
+        # Tell it what file you want to process
+        filename = '/api/mapping/data/value_frequency_test_data.csv'
+
+        # Run the test on filename
+        # This function is in services.py
+        result = process_scan_report_sheet_table(filename)
+
+        self.assertIs(4, len(result))
 
     def test_run_usagi(self):
         x = run_usagi()
