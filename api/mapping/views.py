476d3c2ea54f51af4404308b6adc5b8118e4ba59
David <dschlessinger@live.com>
2021-03-02 10:19:18 +0000

    added a field for users to indicate whether they want to pass a given source value directly into mapping

--- api/mapping/views.py
+++ api/mapping/views.py
@@ -107,6 +107,7 @@ class ScanReportFieldUpdateView(UpdateView):
         'is_patient_id',
         'is_date_event',
         'is_ignore',
+        'pass_from_source',
         'classification_system',
     ]
 
