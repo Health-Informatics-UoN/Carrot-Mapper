f0e9c9a67d6e706d44ac4d41fbfedc99df7e2590
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2021-03-25 15:31:19 +0000

    delete model if ignore and classification columns are not in the scan report

--- api/mapping/services.py
+++ api/mapping/services.py
@@ -4,6 +4,8 @@ import subprocess
 import sys
 import os
 import pandas as pd
+from django.contrib import messages
+
 
 from .models import (
     ScanReport,
@@ -106,53 +108,57 @@ def process_scan_report(scan_report_id):
                 # [:31] is because excel is a pile of s***
                 # - sheet names are truncated to 31 characters
                 name = row[0][:31]
-
-                scan_report_table, _ = ScanReportTable.objects.get_or_create(
-                    scan_report=scan_report,
-                    name=name,
-                )
+                if len(row)<10:
+                    scan_report = ScanReport.objects.get(pk=scan_report_id)
+                    scan_report.delete()
+                if len(row)>=11:
+                    upload=True
+                    scan_report_table, _ = ScanReportTable.objects.get_or_create(
+                        scan_report=scan_report,
+                        name=name,
+                    )
                 
-                # Add each field in Field Overview to the model ScanReportField
-                scanreport=ScanReportField.objects.create(
-                scan_report_table=scan_report_table,
-                name=row[1],
-                description_column=row[2],
-                type_column=row[3],
-                max_length=row[4],
-                nrows=row[5],
-                nrows_checked=row[6],
-                fraction_empty=row[7],
-                nunique_values=row[8],
-                fraction_unique=row[9],
-                ignore_column=row[10],
-                is_patient_id = False,
-                is_date_event=False,
-                is_ignore=False,
-                pass_from_source=False,
-                classification_system=row[11]
-                )
-                if scanreport.ignore_column=='PatientID':
-                    scanreport.is_patient_id=True
-                else: 
-                    scanreport.is_patient_id=False
+                    # Add each field in Field Overview to the model ScanReportField
+                    scanreport=ScanReportField.objects.create(
+                    scan_report_table=scan_report_table,
+                    name=row[1],
+                    description_column=row[2],
+                    type_column=row[3],
+                    max_length=row[4],
+                    nrows=row[5],
+                    nrows_checked=row[6],
+                    fraction_empty=row[7],
+                    nunique_values=row[8],
+                    fraction_unique=row[9],
+                    ignore_column=row[10],
+                    is_patient_id = False,
+                    is_date_event=False,
+                    is_ignore=False,
+                    pass_from_source=False,
+                    classification_system=row[11]
+                    )
+                    if scanreport.ignore_column=='PatientID':
+                        scanreport.is_patient_id=True
+                    else: 
+                        scanreport.is_patient_id=False
 
-                if scanreport.ignore_column=='Date':
-                    scanreport.is_date_event=True
-                else: 
-                    scanreport.is_date_event=False
+                    if scanreport.ignore_column=='Date':
+                        scanreport.is_date_event=True
+                    else: 
+                        scanreport.is_date_event=False
 
-                if scanreport.ignore_column=='Ignore':
-                    scanreport.is_ignore=True
-                else: 
-                    scanreport.is_ignore=False
+                    if scanreport.ignore_column=='Ignore':
+                        scanreport.is_ignore=True
+                    else: 
+                        scanreport.is_ignore=False
 
-                if scanreport.ignore_column=='PassSource':
-                    scanreport.pass_from_source=True
-                else: 
-                    scanreport.pass_from_source=False
+                    if scanreport.ignore_column=='PassSource':
+                        scanreport.pass_from_source=True
+                    else: 
+                        scanreport.pass_from_source=False
 
-                scanreport.save()
-
+                    scanreport.save()
+                    
     # For sheets past the first two in the scan Report
     # i.e. all 'data' sheets that are not Field Overview and Table Overview
     for idxsheet, sheet in enumerate(xlsx.workbook.sheets):
@@ -212,7 +218,7 @@ def process_scan_report(scan_report_id):
                 source_value=ScanReportValue.objects.latest("id"),
                 definition_fixed=False,
             )
-
+    return upload
 
 def build_usagi_index():
     # Use 'build' to create index for the first time.
