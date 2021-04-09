97b31791789b3ae69b9f34e001b60eca274c21c7
calmacx <calmacx@gmail.com>
2021-04-09 12:51:29 +0100

    getting things configured cleaning up warnings

--- api/mapping/views.py
+++ api/mapping/views.py
@@ -64,6 +64,8 @@ from coconnect.tools import mapping_pipeline_helpers
 
 #not sure this is the best practice to have this as a global
 #everytime something imports views.py this is going to get called
+#now that it looks up from the OMOPDb it can take a long time to init
+#~20 seconds, it used to be ~0.1 seconds when loading from .csv
 from coconnect.tools.omop_db_inspect import OMOPDetails
 omop_lookup = OMOPDetails()
 
@@ -542,9 +544,8 @@ class StructuralMappingTableListView(ModelFormSetView):
                                               .get(table__table=destination_table,
                                                    field=unmapped_field)
                         
-                    except: #mapping.models.OmopField.DoesNotExist:
+                    except Exception as err: #mapping.models.OmopField.DoesNotExist:
                         messages.warning(request,f'{destination_table}::{unmapped_field} is somehow misssing??')
-
                         continue
 
                     #attempt to find person_ids and date events
@@ -594,8 +595,15 @@ class StructuralMappingTableListView(ModelFormSetView):
                         omop_field = OmopField.objects\
                                               .get(table__table=destination_table,
                                                    field=destination_field)
-                    except: #mapping.models.OmopField.DoesNotExist:
-                        messages.warning(request,f'{destination_table}::{unmapped_field} is somehow misssing??')
+                    except Exception as err:
+                        #remove message warnings about these now
+                        #rules are automatically generated for:
+                        # <domain>_source_value, <domain>_source_concept_id and <domain>_concept_id
+                        #sometimes one of these might not exist, e.g. for specimen
+                        #do not make a warning about this
+                        if all(x not in destination_field
+                               for x in ['_source_value','_source_concept_id','_concept_id']):
+                            messages.warning(request,f'{destination_table}::{unmapped_field} is somehow misssing??')
                         continue
 
 
