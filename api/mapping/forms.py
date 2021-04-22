c095eda1e1b5d2a06002d0d718aef453334a2f38
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2021-04-22 14:23:13 +0100

    run autopep8 on forms

--- api/mapping/forms.py
+++ api/mapping/forms.py
@@ -25,36 +25,42 @@ class ScanReportForm(forms.Form):
         label="WhiteRabbit ScanReport",
         widget=forms.FileInput(attrs={"class": "form-control"}),
     )
+
     def clean_scan_report_file(self):
-        xlsx = Xlsx2csv(self.cleaned_data['scan_report_file'], outputencoding="utf-8")
+        xlsx = Xlsx2csv(
+            self.cleaned_data['scan_report_file'], outputencoding="utf-8")
 
         filepath = "/tmp/{}.csv".format(xlsx.workbook.sheets[0]["name"])
         xlsx.convert(filepath)
 
         with open(filepath, "rt") as f:
             reader = csv.reader(f)
-            csv_header=next(reader)  # Get header row
-            set_header=['Table', 'Field', 'Description', 'Type', 'Max length', 'N rows', 'N rows checked', 'Fraction empty', 'N unique values', 'Fraction unique', 'Flag', 'Classification']
-            if set(set_header)==set(csv_header):
+            csv_header = next(reader)  # Get header row
+            set_header = ['Table', 'Field', 'Description', 'Type', 'Max length', 'N rows', 'N rows checked',
+                          'Fraction empty', 'N unique values', 'Fraction unique', 'Flag', 'Classification']
+            if set(set_header) == set(csv_header):
                 for row in reader:
-                    flag_column=row[10]
-                    flag_column=flag_column.lower()
-                    classification_column=row[11]
-                    if (flag_column=='patientid') or (flag_column=='date') or (flag_column==''):
+                    flag_column = row[10]
+                    flag_column = flag_column.lower()
+                    classification_column = row[11]
+                    if (flag_column == 'patientid') or (flag_column == 'date') or (flag_column == ''):
                         pass
                     else:
-                        raise (forms.ValidationError( "Check Flag column values. Valid options are: 'PatientID', 'Date' or blank"))
-                    
-                    if (classification_column in VOCABULARY_CHOICES.values()) or (classification_column==''):
+                        raise (forms.ValidationError(
+                            "Check Flag column values. Valid options are: 'PatientID', 'Date' or blank"))
+
+                    if (classification_column in VOCABULARY_CHOICES.values()) or (classification_column == ''):
                         pass
                     else:
-                        raise (forms.ValidationError( "Check Classification column values. Valid options are:{}".format(list(VOCABULARY_CHOICES.values()))))
-                
+                        raise (forms.ValidationError("Check Classification column values. Valid options are:{}".format(
+                            list(VOCABULARY_CHOICES.values()))))
+
                 return self.cleaned_data['scan_report_file']
             else:
-                raise (forms.ValidationError( "Please check the following columns exist in the Scan Report: Table, Field, Description, Type, Max length, N rows, N rows checked, Fraction empty, N unique values, Fraction unique, Flag, Classification."))
-            
-            
+                raise (forms.ValidationError(
+                    "Please check the following columns exist in the Scan Report: Table, Field, Description, Type, Max length, N rows, N rows checked, Fraction empty, N unique values, Fraction unique, Flag, Classification."))
+
+
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(
         required=True, label="Email", error_messages={"exists": "Oops"}
@@ -73,7 +79,8 @@ class UserCreateForm(UserCreationForm):
 
     def clean_email(self):
         if User.objects.filter(email=self.cleaned_data["email"]).exists():
-            raise ValidationError(self.fields["email"].error_messages["exists"])
+            raise ValidationError(
+                self.fields["email"].error_messages["exists"])
         return self.cleaned_data["email"]
 
 
@@ -120,14 +127,17 @@ class DocumentForm(forms.Form):
     )
 
     def clean_document_file(self):
-        data_dictionary_csv = self.cleaned_data['document_file'].read().decode("utf-8-sig").splitlines()[0]
+        data_dictionary_csv = self.cleaned_data['document_file'].read().decode(
+            "utf-8-sig").splitlines()[0]
         header = data_dictionary_csv.split(',')
-        column_names = ["Table Name", "Column Name", "Column Description", "ValueCode", "ValueDescription"]
+        column_names = ["Table Name", "Column Name",
+                        "Column Description", "ValueCode", "ValueDescription"]
 
         if set(column_names) == set(header):
             return self.cleaned_data['document_file']
         else:
-            raise (forms.ValidationError("Please check your column names in your data dictionary"))
+            raise (forms.ValidationError(
+                "Please check your column names in your data dictionary"))
 
 
 class DocumentFileForm(forms.Form):
@@ -144,30 +154,34 @@ class DocumentFileForm(forms.Form):
 
     def clean_document_file(self):
 
-        data_dictionary_csv = self.cleaned_data['document_file'].read().decode("utf-8-sig").splitlines()[0]
+        data_dictionary_csv = self.cleaned_data['document_file'].read().decode(
+            "utf-8-sig").splitlines()[0]
         header = data_dictionary_csv.split(',')
-        column_names = ["Table Name", "Column Name", "Column Description", "ValueCode", "ValueDescription"]
+        column_names = ["Table Name", "Column Name",
+                        "Column Description", "ValueCode", "ValueDescription"]
 
         if set(column_names) == set(header):
             return self.cleaned_data['document_file']
         else:
-            raise (forms.ValidationError("Please check your column names in your data dictionary"))
+            raise (forms.ValidationError(
+                "Please check your column names in your data dictionary"))
 
 
 class DictionarySelectForm(forms.Form):
     document = forms.ModelChoiceField(label="Data Dictionary Document",
-                                      queryset=DocumentFile.objects.filter(status__icontains="Live"),
+                                      queryset=DocumentFile.objects.filter(
+                                          status__icontains="Live"),
                                       to_field_name="document")
 
 
 class ScanReportAssertionForm(forms.Form):
-    negative_assertion=forms.CharField(
-         label="Negative Assertions",
-         widget=forms.TextInput(attrs={"class": "form-control"}),
-     )    
+    negative_assertion = forms.CharField(
+        label="Negative Assertions",
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+    )
 
 
 class NLPForm(forms.Form):
     user_string = forms.CharField(
-        label = ""
+        label=""
     )
