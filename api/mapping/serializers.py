1a3fa707409ec89fd57a27226f0e0908b3d3c73e
shahzadmumtaz22 <53081016+shahzadmumtaz22@users.noreply.github.com>
2021-05-27 13:04:40 +0500

    Update serializers.py

--- api/mapping/serializers.py
+++ api/mapping/serializers.py
@@ -15,11 +15,21 @@ from mapping.models import (
     ScanReport,
     ScanReportTable,
     ScanReportConcept,
+    Mapping,
+    ClassificationSystem,
+    DataDictionary,
+    Document,
+    DocumentFile,
+    DataPartner,
+    OmopField,
+    OmopTable,
+    StructuralMappingRule,
+    Source,
     )
 
-class ScanReportFieldSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ScanReportField
+#class ScanReportFieldSerializer(serializers.HyperlinkedModelSerializer):
+#    class Meta:
+#        model = ScanReportField
 
 
 class ConceptSerializer(serializers.ModelSerializer):
@@ -86,3 +96,53 @@ class ScanReportConceptSerializer(serializers.ModelSerializer):
     class Meta:
         model=ScanReportConcept
         fields='__all__'        
+        
+class MappingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Mapping
+        fields='__all__'      
+
+class ClassificationSystemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ClassificationSystem
+        fields='__all__'               
+
+class DataDictionarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=DataDictionary
+        fields='__all__'               
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Document
+        fields='__all__'               
+
+class DocumentFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=DocumentFile
+        fields='__all__'               
+
+class DataPartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=DataPartner
+        fields='__all__'               
+
+class OmopFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=OmopField
+        fields='__all__'               
+
+class OmopTableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=OmopTable
+        fields='__all__'               
+
+class StructuralMappingRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=StructuralMappingRule
+        fields='__all__' 
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Source
+        fields='__all__'         
