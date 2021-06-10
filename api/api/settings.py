409bcc3c2fef7f29b585455da7cc0d66fe588d03
calmacx <calmacx@gmail.com>
2021-06-10 15:07:12 +0100

    remove inclusion of omop extra db

--- api/api/settings.py
+++ api/api/settings.py
@@ -96,14 +96,6 @@ DATABASES = {
         'NAME':  os.getenv('COCONNECT_DB_NAME'),
         'USER': os.getenv('COCONNECT_DB_USER'),
         'PASSWORD': os.getenv('COCONNECT_DB_PASSWORD'),
-    },
-    'omop': {
-        'ENGINE': os.getenv('OMOP_POSTGRES_ENGINE'),
-        'HOST': os.getenv('OMOP_POSTGRES_HOST'),
-        'USER': os.getenv('OMOP_POSTGRES_USER'),
-        'PASSWORD': os.getenv('OMOP_POSTGRES_PASSWORD'),
-        'PORT': os.getenv('OMOP_POSTGRES_PORT'),
-        'NAME':  os.getenv('OMOP_POSTGRES_DB')
     }
 }
 
