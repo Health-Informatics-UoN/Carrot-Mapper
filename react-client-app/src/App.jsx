ec34931ff617073f8ebe122aa97594e3e4b7c461
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-06-11 16:36:53 +0100

    Added storybook and remove app/

--- react-client-app/src/App.jsx
+++ react-client-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react"
-import Btn from '../components/Btn'
+import Btn from './components/Btn'
 
-export const App = () => {
+const App = () => {
     return (
         <ChakraProvider>
             <Btn text='Test props'/>
       </ChakraProvider>
    )
 }
+
+export default App;
