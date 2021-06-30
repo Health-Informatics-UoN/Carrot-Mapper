6ba1030c9c89fa0acf67739cda4f400f5d3a7968
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2021-06-30 15:59:11 +0100

    fix conflict

--- api/mapping/views.py
+++ api/mapping/views.py
@@ -630,7 +630,7 @@ class ScanReportFormView(FormView):
         
         queue = QueueClient.from_connection_string(
             conn_str=os.environ.get("STORAGE_CONN_STRING"),
-            queue_name="scanreports"
+            queue_name=os.environ.get("SCAN_REPORT_QUEUE_NAME")
             #change to scanreports-local for local development
            
         )
