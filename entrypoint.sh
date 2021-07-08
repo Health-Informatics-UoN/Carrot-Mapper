002a5a6659fd3246c1e4f10961d22c07dc9d4772
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-08 16:17:49 +0100

    Remove debugging output.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,9 @@
 wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready! Listening on ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT}"
 
 cd /api
-echo "rm"
+
 rm -rf staticfiles/{*,.*}
-echo "rm-ed"
-#echo "mkdir"
-#mkdir staticfiles
-#echo "mkdir-ed"
+
 python /api/manage.py collectstatic
 
 python /api/manage.py runserver 0.0.0.0:8000
