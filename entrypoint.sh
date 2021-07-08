1c821534d6d98e0615e9379baa5ba5929faeb9ff
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-08 15:47:45 +0100

    Don't remove the whole directory, just clear it of all contents.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready
 
 cd /api
 echo "rm"
-rm -rf staticfiles/
+rm -rf staticfiles/*
 echo "rm-ed"
-echo "mkdir"
-mkdir staticfiles
-echo "mkdir-ed"
+#echo "mkdir"
+#mkdir staticfiles
+#echo "mkdir-ed"
 python /api/manage.py collectstatic
 
 python /api/manage.py runserver 0.0.0.0:8000
