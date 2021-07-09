63ab421aa8d32eefd305f9f7ab2a703d60400eda
Joseph Lavagna <josephlavagna1998@gmail.com>
2021-07-09 13:48:08 +0100

    Additional updates to GET concepts

--- react-client-app/src/api/values.js
+++ react-client-app/src/api/values.js
@@ -19,38 +19,76 @@ function useValue() {
 
 const api = 'http://127.0.0.1:8080/api'
 
+const fetcher = async (...args) =>{
+    const response = await fetch(...args);
+    return await response.json();
+}
+
 /* Fetches all scan report values for a specific scan report field id */
-function useScanReportValues({ id }) {
-    const { data, error, revalidate } = useSWR(`${api}/scanreportvaluesfilter/?scan_report_field=${id}`)
+function useScanReportValues(id) {
+    const { data, error } = useSWR(`${api}/scanreportvaluesfilter/?scan_report_field=${id}`, fetcher)
     return {
         data: data,
         isLoading: !error && !data,
-        isError: error,
-        revalidate: revalidate
+        isError: error
     }
 }
 
 /* Fetches all scan concepts for a specific scan report value id */
-function useScanReportConcepts() {
-    const { data, error, revalidate } = useSWR(`${api}/scanreportconceptsfilter/?object_id=841984`)
+function useScanReportConcepts(id) {
+    const { data, error } = useSWR(`${api}/scanreportconceptsfilter/?object_id=${id}`, fetcher)
     return {
         data: data,
         isLoading: !error && !data,
-        isError: error,
-        revalidate: revalidate
+        isError: error
     }
 }
+
 
 /* Fetches concept object for specific concept id */
-function useConcepts({ id }) {
-    const { data, error, revalidate } = useSWR(`${api}/omop/concepts/${id}`)
+function useConcepts(id) {
+    const { data, error } = useSWR(`${api}/omop/concepts/${id}`, fetcher)
     return {
         data: data,
         isLoading: !error && !data,
-        isError: error,
-        revalidate: revalidate
+        isError: error
     }
 }
 
+const getScanReportValues = async (id) => {
+    const response = await fetch(`${api}/scanreportvaluesfilter/?scan_report_field=${id}`);
+    const data =  await response.json();
+    return data;
+}
+const getScanReportConcepts = async (id) => {
+    const response = await fetch(`${api}/scanreportconceptsfilter/?object_id=${id}`);
+    return await response.json();
+}
+const getConcepts = async (id) => {
+    const response = await fetch(`${api}/omop/concepts/${id}`);
+    return await response.json();
+}
 
-export { useValue, useScanReportValues, useScanReportConcepts, useConcepts }
+// This works - not using conf
+function testing(id) {
+    getScanReportValues(8378).then(values => {
+        values.map((value) => {
+            getScanReportConcepts(value.id).then(concepts => {
+                concepts.map((concept) => {
+                    getConcepts(concept.concept).then(con => {
+                        console.log(con)
+                    })
+                })
+            })
+        })
+    })
+}
+
+/* 
+(1) Need to call useScanReportValues hook 
+    Once complete update state?
+(2) On ScanReportValue state change call useScanReportConcepts for each scan report value
+    Once complete again update state
+(3) */
+
+export { useValue, useScanReportValues, useScanReportConcepts, useConcepts, getScanReportValues, getScanReportConcepts, getConcepts }
