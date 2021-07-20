d70b7c9bab289308a49700e02f4abe0c60f51b04
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-20 16:57:24 +0100

    Add ls before and after collectstatic.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ cd /api
 
 rm -rf staticfiles/
 mkdir staticfiles
-
+ls staticfiles
 python /api/manage.py collectstatic
-
+ls staticfiles
 # Set tmp dir to be in-memory for speed. Pass logs to stdout/err as Docker will expect them there
 gunicorn --worker-tmp-dir /dev/shm --log-file=- --bind :8000 --workers 3 api.wsgi:application
