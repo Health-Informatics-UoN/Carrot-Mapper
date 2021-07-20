371cc99ac0c68063eca994354caa0fb2c94907cf
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-20 11:26:39 +0100

    Delete the whole of api/staticfiles/ and recreate the directory afresh - this will avoid the 'rm cannot remove .' warnings.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready
 
 cd /api
 
-rm -rf staticfiles/{*,.*}
+rm -rf staticfiles/
+mkdir staticfiles
 
 python /api/manage.py collectstatic
 
