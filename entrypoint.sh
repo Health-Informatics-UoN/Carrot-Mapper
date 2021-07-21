1fe773d5bf3b977b9e648f179c5cce8223f3ed69
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-21 09:10:42 +0100

    Add further debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready! Listening on ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT}"
 
 cd /api
-
+echo "rm"
 rm -rf staticfiles/
+echo "mkdir"
 mkdir staticfiles
 ls staticfiles
 python /api/manage.py collectstatic
