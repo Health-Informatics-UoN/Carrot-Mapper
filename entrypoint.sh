7392df5f1afede743530645c621a22e55f761717
Sam Cox <sam.cox@nottingham.ac.uk>
2021-07-21 11:26:01 +0100

    Tidy up to final form.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-echo "wait"
+
 wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready! Listening on ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT}"
 
 cd /api
-ls -al
-echo "rm"
 rm -rf staticfiles
-echo "mkdir"
 mkdir staticfiles
-ls staticfiles
+
 python /api/manage.py collectstatic
-ls staticfiles
+
 # Set tmp dir to be in-memory for speed. Pass logs to stdout/err as Docker will expect them there
 gunicorn --worker-tmp-dir /dev/shm --log-file=- --bind :8000 --workers 3 api.wsgi:application
