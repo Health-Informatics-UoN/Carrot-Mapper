f64e615e8a31caa57d9c307dfb054ce5f7e114ba
Samad <eeysaad@nottingham.ac.uk>
2021-08-18 16:05:38 +0100

    removed unneccessary files

--- react-client-app/src/App.jsx
+++ react-client-app/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react"
 import styles from './styles'
 import DataTbl from './components/DataTbl'
-import Test from './components/Test'
-import Test2 from './components/Test2'
 
 import PageHeading from './components/PageHeading'
 
