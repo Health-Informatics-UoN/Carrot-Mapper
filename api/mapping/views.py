e475bac14777c07e785368754505e9be64a0c945
shahzadmumtaz22 <53081016+shahzadmumtaz22@users.noreply.github.com>
2021-09-15 20:17:09 +0500

    Update views.py

--- api/mapping/views.py
+++ api/mapping/views.py
@@ -177,7 +177,13 @@ class DrugStrengthViewSet(viewsets.ReadOnlyModelViewSet):
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset=User.objects.all()
+    serializer_class=UserSerializer
+
+class UserFilterViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset=User.objects.all()
     serializer_class=UserSerializer    
+    filter_backends=[DjangoFilterBackend]
+    filterset_fields={'id':['in', 'exact']}
     
 class ScanReportViewSet(viewsets.ModelViewSet):
     queryset=ScanReport.objects.all()
