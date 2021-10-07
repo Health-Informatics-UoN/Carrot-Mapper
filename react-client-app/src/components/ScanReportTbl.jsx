b6486e0ae035a0e2ed9a72079f554fe2762f333c
Samad <eeysaad@nottingham.ac.uk>
2021-10-07 15:38:52 +0100

    added outline

--- react-client-app/src/components/ScanReportTbl.jsx
+++ react-client-app/src/components/ScanReportTbl.jsx
@@ -295,7 +295,7 @@ const ScanReportTbl = (props) => {
                                                 <Flex marginLeft="10px">Loading status</Flex>
                                             </Flex>
                                             :
-                                            <Select minW="max-content" style={{ fontWeight: "bold" }} variant="unstyled" value={item.status} onChange={(option) => setStatus(item.id, option.target.value)}>
+                                            <Select minW="max-content" style={{ fontWeight: "bold" }} variant="outline" value={item.status} onChange={(option) => setStatus(item.id, option.target.value)}>
                                                 <option style={{ fontWeight: "bold" }} disabled>Status</option>
                                                 {statuses.map((item, index) =>
                                                     <option key={index} value={item}>{mapStatus(item)}</option>
