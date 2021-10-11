0410d4f0550c11e09eacb395451d8eb895c36515
Samad <eeysaad@nottingham.ac.uk>
2021-10-11 09:09:07 +0100

    added comma

--- api/api/react.py
+++ api/api/react.py
@@ -4,6 +4,6 @@ from mapping.models import Status
 def react(request):
     return {
         "a": os.environ.get("COCONNECT_DB_AUTH_TOKEN"),
-        "u": os.environ.get("CCOM_APP_URL")   
+        "u": os.environ.get("CCOM_APP_URL"),   
         "status":[{'id': year[0], 'label': year[1]} for year in Status.choices]
     }
