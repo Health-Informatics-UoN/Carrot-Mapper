edfcb5f7a7453b0577510c98c6a624853860c80a
Samad <eeysaad@nottingham.ac.uk>
2021-10-11 09:58:40 +0100

    changed react.py variable name

--- api/api/react.py
+++ api/api/react.py
@@ -5,5 +5,5 @@ def react(request):
     return {
         "a": os.environ.get("COCONNECT_DB_AUTH_TOKEN"),
         "u": os.environ.get("CCOM_APP_URL"),   
-        "status":[{'id': year[0], 'label': year[1]} for year in Status.choices]
+        "status":[{'id': id, 'label': label} for id, label in Status.choices]
     }
