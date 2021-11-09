7a58f90d9bebc9a87adc8fe104bab9c544a13979
Temp <temp@Temps-MacBook-Air.local>
2021-11-09 13:00:55 +0000

    retrieving content_object_id and content_type_id

--- api/mapping/management/commands/create_concept.py
+++ api/mapping/management/commands/create_concept.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand, CommandError
-from data.models import concept
 from data.models.concept import Concept
 from mapping.models import ScanReportConcept, ScanReportField, ScanReportValue
 from mapping.services_rules import (
@@ -17,32 +16,10 @@ class Command(BaseCommand):
 
         male = 8507
 
-        querie_id = ScanReportConcept.objects.filter(concept_id=8507)
-        # queries for
-        for scan_report_concept in querie_id:
-            content_object = scan_report_concept.content_object
-
-            print(scan_report_concept.object_id,
-                  scan_report_concept.content_type_id)
-
-            if isinstance(content_object, ScanReportValue):
-                scan_report_value = content_object
-                scan_report_field = scan_report_value.scan_report_field
-                print(scan_report_value.value, scan_report_value.value_description,
-                      scan_report_value.scan_report_field_id, scan_report_field.name)
-
-
-class Command(BaseCommand):
-    help = 'create rules from existing concepts found for a given table id'
-
-    def handle(self, *args, **options):
-
-        male = 8507
-
         # queries ScanReportConcept table for rows where concept_id=8507
-        querie_id = ScanReportConcept.objects.filter(concept_id=8507)
+        query_id = ScanReportConcept.objects.filter(concept_id=male)
 
-        for scan_report_concept in querie_id:
+        for scan_report_concept in query_id:
             content_object = scan_report_concept.content_object
 
             # print object_ids and content_type_ids
@@ -50,10 +27,22 @@ class Command(BaseCommand):
                   scan_report_concept.content_type_id)
             # If code ends here, it prints out only the object_ids and content_type_ids associated with the concept_id 8507
 
-            if isinstance(content_object, ScanReportValue):
-                scan_report_value = content_object
-                scan_report_field = scan_report_value.scan_report_field
+            scan_report_value = content_object
+            scan_report_field = scan_report_value.scan_report_field
 
-                # grab the value, value_description and scan_report_field_id from ScanReportValue. And field_name from ScanReportField.
-                print(scan_report_value.value, scan_report_value.value_description,
-                      scan_report_value.scan_report_field_id, scan_report_field.name)
+           # define lists
+            scan_report_value_list = [scan_report_value.value, scan_report_value.value_description,
+                                      scan_report_value.scan_report_field_id]
+
+            scan_report_field_list = [scan_report_field.name]
+
+            if isinstance(content_object, ScanReportValue):
+
+                # if content_object is scanreportvalue, grab the value, value_description, and scan_report_field_id from ScanReportValue.
+                scan_report_value_list.append(content_object)
+
+            elif isinstance(content_object, ScanReportField):
+                # But if content_object is scanreportfield, just grab the scanreportfield name.
+                scan_report_field_list.append(content_object)
+
+        print(scan_report_value_list, scan_report_field_list)
