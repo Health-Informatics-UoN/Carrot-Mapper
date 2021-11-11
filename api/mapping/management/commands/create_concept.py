89a01b92cb0258a9da7a356e895a1ff8e17d3dc5
Temp <temp@Temps-MacBook-Air.local>
2021-11-11 12:12:23 +0000

    retrieving object_ids and content_type_ids

--- api/mapping/management/commands/create_concept.py
+++ api/mapping/management/commands/create_concept.py
@@ -15,34 +15,31 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         male = 8507
-
+        Retlist = []
         # queries ScanReportConcept table for rows where concept_id=8507
-        query_id = ScanReportConcept.objects.filter(concept_id=male)
+        query_id = ScanReportConcept.objects.filter(concept=male)
 
         for scan_report_concept in query_id:
             content_object = scan_report_concept.content_object
 
-            # print object_ids and content_type_ids
-            print(scan_report_concept.object_id,
-                  scan_report_concept.content_type_id)
+            # print object_ids and content_type_ids and append to Retlist.
+            Retlist.extend([scan_report_concept.object_id,
+                           scan_report_concept.content_type_id])
             # If code ends here, it prints out only the object_ids and content_type_ids associated with the concept_id 8507
 
-            scan_report_value = content_object
-            scan_report_field = scan_report_value.scan_report_field
+            scan_report_values = ScanReportValue.objects.filter(
+                conceptID=male)
+            # I have had to use this conceptID because ScanReportValues are not callable using the object_id and content_type_id retrieved above. Comes back with an error.
+            for scan_report_value in scan_report_values:
+             # if content_object is scanreportvalue, grab the value, value_description, and scan_report_field_id and append to Retlist.
+                Retlist.extend([scan_report_value.value, scan_report_value.value_description,
+                               scan_report_value.scan_report_field_id])
 
-           # define lists
-            scan_report_value_list = [scan_report_value.value, scan_report_value.value_description,
-                                      scan_report_value.scan_report_field_id]
+             # using the scan_report_field_id of 157 retrieved above, search the ScanReportField for field name and append fieldname to Retlist
+            scan_report_fields = ScanReportField.objects.filter(
+                id=157)
+            for scan_report_field in scan_report_fields:
+                Retlist.extend([scan_report_field.name])
 
-            scan_report_field_list = [scan_report_field.name]
-
-            if isinstance(content_object, ScanReportValue):
-
-                # if content_object is scanreportvalue, grab the value, value_description, and scan_report_field_id from ScanReportValue.
-                scan_report_value_list.append(content_object)
-
-            elif isinstance(content_object, ScanReportField):
-                # But if content_object is scanreportfield, just grab the scanreportfield name.
-                scan_report_field_list.append(content_object)
-
-        print(scan_report_value_list, scan_report_field_list)
+            print(Retlist)
+            # print(scan_report_field.name)
