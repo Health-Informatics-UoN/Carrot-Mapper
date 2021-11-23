12b3cf13569c91002958f1c59d39ca1ef2020720
Samad <eeysaad@nottingham.ac.uk>
2021-11-23 11:44:34 +0000

    added comments

--- api/mapping/management/commands/create_concept.py
+++ api/mapping/management/commands/create_concept.py
@@ -18,32 +18,38 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         use_archived = options['use_archived']
-        concept_id = options['concept_id'] 
+        concept_id = options['concept_id']
+        #get list of scan report concepts to loop through
         scan_report_concepts = ScanReportConcept.objects.filter(concept=concept_id) 
+        # create list variable to populate with tuples
         finalList = []
-        
+        # loop through scan report concepts
         for scan_report_concept in scan_report_concepts:
             if (scan_report_concept.content_type_id==17):
-                # If it is for a value
-                if (use_archived):         
+                # If the scan report concept is for a value
+                if (use_archived):#if archived scanreports are included, get all values with the object id from the scan report concept    
                     scan_report_values = ScanReportValue.objects.filter(pk=scan_report_concept.object_id)
                 else:
+                    #if archived scanreports are excluded, add a filter to remove archived reports from results
                     scan_report_values = ScanReportValue.objects.filter(
                         pk=scan_report_concept.object_id,
                         scan_report_field__scan_report_table__scan_report__hidden=False)
+                #if there are any values returned by the query, get the field for the value and add tuple to final list
                 if scan_report_values.exists():
                         scan_value = scan_report_values[0]
                         scan_report_fields = ScanReportField.objects.filter(pk=scan_value.scan_report_field_id)
                         scan_field = scan_report_fields[0]
                         finalList.append((scan_field.name ,scan_value.value,scan_value.value_description))
             elif (scan_report_concept.content_type_id==15):
-                # If it is for a field
-                if (use_archived):  
+                # If the scan report concept is for a field
+                if (use_archived):#if archived scanreports are included, get all fields with the object id from the scan report concept 
                     scan_report_fields = ScanReportField.objects.filter(pk=scan_report_concept.object_id)
                 else:
+                    #if archived scanreports are excluded, add a filter to remove archived reports from results
                     scan_report_fields = ScanReportField.objects.filter(
                         pk=scan_report_concept.object_id,
                         scan_report_table__scan_report__hidden=False)
+                #if there are any fields returned by the query, add tuple to final list
                 if scan_report_fields.exists():
                     scan_field = scan_report_fields[0]
                     finalList.append((scan_field.name ,None,None))
