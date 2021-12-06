e3dd147cf7d0d63f17b1d7364742f09a10b3ab94
Samad <eeysaad@nottingham.ac.uk>
2021-12-06 10:06:14 +0000

    made mapping table code more reusable

--- react-client-app/src/components/MappingTbl.jsx
+++ react-client-app/src/components/MappingTbl.jsx
@@ -22,6 +22,7 @@ import { useGet } from '../api/values'
 import ConceptTag from './ConceptTag'
 import MappingModal from './MappingModal'
 import SummaryTbl from './SummaryTbl'
+import RulesTbl from './RulesTbl'
 
 
 
@@ -202,16 +203,7 @@ const MappingTbl = () => {
                 <Button variant="blue" isLoading={isDownloadingCSV} loadingText="Downloading" spinnerPlacement="start" onClick={() => { window.downloadCSV(setDownloadingCSV) }}>Download Mapping CSV</Button>
                 <Button variant="blue" onClick={onOpen}>Show Summary view</Button>
             </HStack>
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-                <div style={{ fontWeight: "bold", marginRight: "10px" }} >Filters: </div>
-                {filters.map((filter, index) => {
-                    return (
-                        <div style={{ marginTop: "10px" }}>
-                            <ConceptTag key={index} conceptName={filter.name} conceptId={filter.title} conceptIdentifier={filter.name} itemId={filter.title} handleDelete={removeFilter} />
-                        </div>
-                    )
-                })}
-            </div>
+            
             <div>
                 {mapDiagram.showing &&
                     <>
@@ -236,85 +228,10 @@ const MappingTbl = () => {
             {error ?
                 <div>{error}</div>
                 :
-                <Table variant="striped" colorScheme="greyBasic">
-                    <TableCaption></TableCaption>
-                    <Thead>
-                        <Tr>
-                            <Th>Rule ID</Th>
-                            <Th>
-                                <Select minW="130px" style={{ fontWeight: "bold" }} variant="unstyled" value="Destination Table" readOnly onChange={(option) => setDestinationFilter(option.target.value)}>
-                                    <option style={{ fontWeight: "bold" }} disabled>Destination Table</option>
-                                    <>
-                                        {[...[...new Set(values.map(data => data.destination_table.name))]].sort((a, b) => a.localeCompare(b))
-                                            .map((item, index) =>
-                                                <option key={index} value={item}>{item}</option>
-                                            )}
-                                    </>
-                                </Select>
-                            </Th>
-                            <Th>Destination Field</Th>
-                            <Th>
-                                <Select minW="130px" style={{ fontWeight: "bold" }} variant="unstyled" value="Source Table" readOnly onChange={(option) => setSourceFilter(option.target.value)}>
-                                    <option style={{ fontWeight: "bold" }} disabled>Source Table</option>
-                                    <>
-                                        {[...[...new Set(values.map(data => data.source_table.name))]].sort((a, b) => a.localeCompare(b))
-                                            .map((item, index) =>
-                                                <option key={index} value={item}>{item}</option>
-                                            )}
-                                    </>
-                                </Select>
-                            </Th>
-                            <Th>Source Field</Th>
-                            <Th>Term Map</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {applyFilters(values).length > 0 ?
-                            // Create new row for every value object
-                            applyFilters(values).map((item, index) =>
-                                <Tr key={index}>
-                                    <Td maxW={[50, 100, 200]} >{item.rule_id} </Td>
-                                    <Td maxW={[50, 100, 200]} >{item.destination_table.name} </Td>
-                                    <Td maxW={[50, 100, 200]} >{item.destination_field.name}</Td>
-                                    <Td maxW={[50, 100, 200]} ><Link style={{ color: "#0000FF", }} href={window.u + "fields/?search=" + item.source_table.id}>{item.source_table.name}</Link></Td>
-                                    <Td maxW={[50, 100, 200]} ><Link style={{ color: "#0000FF", }} href={window.u + "values/?search=" + item.source_field.id}>{item.source_field.name}</Link></Td>
-                                    <Td maxW={[50, 100, 200]} >
-                                        {item.term_mapping != null &&
-                                            <>
-                                                {typeof item.term_mapping == "object" ?
-                                                    <VStack>
-                                                        <div>
-                                                            <span style={{ color: "#dd5064", }}>
-                                                                "{Object.keys(item.term_mapping)[0]}"
-                                                            </span>
-                                                            <ArrowForwardIcon />
-                                                            <span style={{ color: "#1d8459", }}>
-                                                                {item.term_mapping[Object.keys(item.term_mapping)[0]]+" "}
-                                                                {item.rule_name}
-                                                            </span>
-                                                        </div>
-                                                    </VStack>
-                                                    :
-                                                    <div>
-                                                        {JSON.stringify(item.term_mapping)} {item.rule_name}
-                                                    </div>
-                                                }
-
-                                            </>
-
-
-                                        }
-                                    </Td>
-                                </Tr>
-
-                            )
-                            :
-                            <Flex padding="30px">
-                                <Flex marginLeft="10px">Nothing</Flex>
-                            </Flex>
-                        }
-                    </Tbody>
-                </Table>
+                <RulesTbl values={values}
+                filters={filters}removeFilter={removeFilter} setDestinationFilter={setDestinationFilter}setSourceFilter={setSourceFilter}
+                destinationTableFilter={destinationTableFilter}sourceTableFilter={sourceTableFilter} applyFilters={applyFilters}/>
+                
             }
         </div>
     );
