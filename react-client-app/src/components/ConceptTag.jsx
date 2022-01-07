0b21632f54c102ac3d6d991511a4448258a76b1d
Samad <eeysaad@nottingham.ac.uk>
2022-01-07 14:04:02 +0000

    edited concept tag to take creation_type field and populate tag accordingly

--- react-client-app/src/components/ConceptTag.jsx
+++ react-client-app/src/components/ConceptTag.jsx
@@ -1,8 +1,8 @@
-import { Tag, Text, TagCloseButton, Wrap } from '@chakra-ui/react';
+import { Tag, Text, TagCloseButton, Wrap, Tooltip } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
-const ConceptTag = ({ conceptId, conceptIdentifier, conceptName, itemId, handleDelete, backgroundColor }) => {
+const ConceptTag = ({ conceptId, conceptIdentifier, conceptName, itemId, handleDelete, backgroundColor, creation_type }) => {
     // pass id of concept and determine background colour depending on the id
     const tagColour = (id) => {
         switch (id) {
@@ -11,25 +11,66 @@ const ConceptTag = ({ conceptId, conceptIdentifier, conceptName, itemId, handleD
             case 111:
                 return '#33cc33'
             default:
-                return backgroundColor
+                if (creation_type) {
+                    let color
+                    switch (creation_type) {
+                        case "vocab":
+                            color = "#ff00ff"
+                            break
+                        case "reuse":
+                            color = "#00ff00"
+                            break
+                        case "manual":
+                            color = "#3C579E"
+                            break
+                        default:
+                            color = '#3C579E'
+                            break;
+                    }
+                    return color
+                }
+                else {
+                    return '#3C579E'
+                }
+
         }
     }
 
+    const mapCreationTypeToString = (type)=>{
+        let str
+        switch (type) {
+            case "vocab":
+                str = "added through vocab"
+                break
+            case "reuse":
+                str = "added through mapping reuse"
+                break
+            case "manual":
+                str = "manually added"
+                break
+            default:
+                str = 'unknown origins'
+                break;
+        }
+        return str
+    }
     return (
-        <Tag
-            size='lg'
-            key={conceptId}
-            borderRadius="full"
-            variant="solid"
-            backgroundColor={tagColour(conceptId)}
-        >
-            <Text w="max-content" ml="8px" py="4px">{conceptId + " " + conceptName}</Text>
-            <Wrap direction='right'>
-                <TagCloseButton onClick={() => { handleDelete(itemId, conceptIdentifier) }} />
-            </Wrap>
+        <Tooltip label={creation_type?`${conceptId} ${conceptName} (${mapCreationTypeToString(creation_type)})`:null}>
+            <Tag
+                size='lg'
+                key={conceptId}
+                borderRadius="full"
+                variant="solid"
+                backgroundColor={tagColour(conceptId)}
+            >
+                <Text w="max-content" ml="8px" py="4px">{conceptId + " " + conceptName} {creation_type ? `(${creation_type.charAt(0)})` : ""}</Text>
+                <Wrap direction='right'>
+                    <TagCloseButton onClick={() => { handleDelete(itemId, conceptIdentifier) }} />
+                </Wrap>
 
 
-        </Tag>
+            </Tag>
+        </Tooltip>
 
     )
 }
@@ -38,10 +79,11 @@ const ConceptTag = ({ conceptId, conceptIdentifier, conceptName, itemId, handleD
 ConceptTag.propTypes = {
     conceptId: PropTypes.string.isRequired,
     backgroundColor: PropTypes.string,
+    creation_type: PropTypes.string,
 };
 
 // Defaults to dark blue
 ConceptTag.defaultProps = {
-    backgroundColor: "#3C579E"
+    //backgroundColor: "#3C579E"
 };
 export default ConceptTag
