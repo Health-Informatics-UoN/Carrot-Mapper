7e01aeaa2f8066390d2aaab6c0d72fdcc116e72d
pszdl <daniel.lea@nottingham.ac.uk>
2022-01-31 12:50:01 +0000

    set up unit test for `ProjectListView`

--- api/mapping/tests.py
+++ api/mapping/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from mapping.models import Project
+
+
+class TestProjectListView(TestCase):
+    def setUp(self):
+        # Set up test users
+        User = get_user_model()
+        self.test_user1 = User(username="Prof. Xavier", password="heiiehjifehr123")
+        self.test_user2 = User(username="Jean Gray", password="vjirjijeijire898")
+        self.test_user1.save()
+        self.test_user2.save()
+
+        # Set up test project
+        self.visible_project = Project(
+            name="Unit test project", members=[self.test_user]
+        )
+
+    def test_retrieved_queryset(self):
+        return False
