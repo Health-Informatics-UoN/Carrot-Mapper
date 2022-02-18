000fb0c9dd279aeb9b137b9db07d24a2ca7d8f20
pszdl <daniel.lea@nottingham.ac.uk>
2022-02-18 14:51:33 +0000

    correct `useLogin`

--- react-client-app/src/api/auth.js
+++ react-client-app/src/api/auth.js
@@ -6,9 +6,16 @@ const loginUrl = `${process.env.CCOM_APP_URL}api-token-auth/`
 const useLogin =  async (username, password) => {
     const response = await fetch(loginUrl, {
         method: "POST",
-        headers: {Authorization: `Basic ${username}:${password}`},
+        body: JSON.stringify(
+            {
+                username: username,
+                password: password,
+            }
+        )
     });
     const token = await response.json()
     // Put the token into a cookie, which will expire after 1 day
     Cookies.set("CCOM_AUTH_TOKEN", token["token"], {expires: 1})
 }
+
+export default useLogin
