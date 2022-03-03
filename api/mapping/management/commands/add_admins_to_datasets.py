9d6f685be46015adc60258e5311329951dc84bdc
pszdl <daniel.lea@nottingham.ac.uk>
2022-03-03 12:05:57 +0000

    logic for adding admins to datasets

--- api/mapping/management/commands/add_admins_to_datasets.py
+++ api/mapping/management/commands/add_admins_to_datasets.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from mapping.models import DataPartner, Dataset
+from mapping.models import Dataset
 
 
 class Command(BaseCommand):
@@ -19,4 +19,20 @@ class Command(BaseCommand):
         and setting the author as an admin.
         """
 
-        pass
+        dataset_names = options.get("datasets")
+
+        if dataset_names:
+            print(f"Finding admins for {len(dataset_names)} dataset(s)...")
+            datasets = Dataset.objects.filter(admins=None, name__in=dataset_names)
+
+        else:
+            print(f"No datasets given. Finding admins for all datasets...")
+            datasets = Dataset.objects.filter(admins=None)
+
+        for ds in datasets:
+            print(f"Adding admins to {ds.name}")
+            authors = [
+                author
+                for author in ds.scan_reports.all().values_list("author", flat=True)
+            ]
+            ds.admins.add(*authors)
