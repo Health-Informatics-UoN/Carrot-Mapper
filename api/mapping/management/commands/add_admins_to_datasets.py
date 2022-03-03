d2ae32ac15f4b50630464abdf8f9bdb031af5017
pszdl <daniel.lea@nottingham.ac.uk>
2022-03-03 11:17:03 +0000

    cmd args for admin command

--- api/mapping/management/commands/add_admins_to_datasets.py
+++ api/mapping/management/commands/add_admins_to_datasets.py
@@ -4,7 +4,15 @@ from mapping.models import DataPartner, Dataset
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            "datasets",
+            default=[],
+            nargs="*",
+            help="""(Optional) The names of the dataset(s) to add an admin.
+            By default find all datasets with no admins and add the authors of the
+            scan reports in the dataset.
+            """,
+        )
 
     def handle(self, *args, **options):
         """Logic for finding the first scan report of a dataset
