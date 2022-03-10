f9f7db51d7f9d02a0aa7c77a5dd0606342bd5346
pszdl <daniel.lea@nottingham.ac.uk>
2022-03-10 14:10:12 +0000

    use `os.path.join` for db path

--- api/api/ci_settings.py
+++ api/api/ci_settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = "api.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
