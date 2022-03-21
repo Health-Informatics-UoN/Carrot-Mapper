a7455601473fd2a9d9ad3aedacc4ec0ee817b7c0
pszdl <daniel.lea@nottingham.ac.uk>
2022-03-21 15:21:59 +0000

    first draft of `CCSelectInput`

---
+++ react-client-app/src/components/CCSelectInput.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+    Box, FormControl, FormLabel, FormErrorMessage, Select
+} from "@chakra-ui/react"
+import ConceptTag from './ConceptTag'
+
+const CCSelectInput = (props) => {
+    /**
+     * Reusable text input component.
+     * Required args:
+     *  id (String): the ID for the input
+     *  label (String): the text for the label
+     */
+    // Check for required arguments
+    if (props.id === undefined) {
+        throw "`id` must be defined."
+    }
+    if (props.label === undefined) {
+        throw "`label` must be defined."
+    }
+    if (props.value === undefined) {
+        throw "`value` must be defined."
+    }
+    // Check handleInput is a function if defined
+    if (props.handleInput !== undefined && typeof (props.handleInput) !== 'function') {
+        throw "`handleInput` must be a function."
+    }
+    return (
+        <FormControl isInvalid={props.formErrors && props.formErrors.length > 0} mt={4}>
+            <FormLabel htmlFor={props.id} w="200px">{props.label}</FormLabel >
+            <Select
+                value={JSON.stringify(props.value)}
+                onChange={
+                    (option) => props.handleInput(JSON.parse(option.target.value))
+                }
+                isReadOnly={props.isReadOnly ? props.isReadOnly : false}
+            >
+                {props.selectOptions.map((item, index) =>
+                    <option key={index} value={JSON.stringify(item)}>{item.name}</option>
+                )}
+            </Select>
+            {props.formErrors && props.formErrors.length > 0 &&
+                <FormErrorMessage>{props.formErrors[0]}</FormErrorMessage>
+            }
+        </FormControl>
+    )
+}
+
+export default CCSelectInput
