f2e8164d3b65ed5c30c366df0708d7c75c1fe7f8
pszdl <daniel.lea@nottingham.ac.uk>
2022-03-21 14:00:56 +0000

    first draft of `CCSwitchInput`

---
+++ react-client-app/src/components/CCSwitchInput.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+    FormControl, FormLabel, Switch
+} from "@chakra-ui/react"
+
+const CCSwitchInput = (props) => {
+    /**
+     * Reusable text input component.
+     * Required args:
+     *  id (String): the ID for the input
+     *  label (String): the text for the label
+     *  checkedMessage (String): the text to show when checked
+     *  notCheckedMessage (String): the text to show when not checked
+     */
+    // Check for required arguments
+    if (props.id === undefined) {
+        throw "`id` must be defined."
+    }
+    if (props.label === undefined) {
+        throw "`label` must be defined."
+    }
+    if (props.checkedMessage === undefined) {
+        throw "`checkedMessage` must be defined."
+    }
+    if (props.notCheckedMessage === undefined) {
+        throw "`notCheckedMessage` must be defined."
+    }
+    // Check handleInput is a function if defined
+    if (props.handleInput !== undefined && typeof (props.handleInput) !== 'function') {
+        throw "`handleInput` must be a function."
+    }
+    return (
+        <FormControl mt={4}>
+            <FormLabel htmlFor={props.id} style={{ fontWeight: "bold" }}>{props.label}</FormLabel>
+            <Flex alignItems={"center"}>
+                <Switch
+                    id={props.id}
+                    isChecked={props.isChecked ? props.isChecked : false}
+                    isReadOnly={props.isReadOnly ? props.isReadOnly : false}
+                    onChange={e => props.handleInput(e.checked)}
+                />
+                <Text
+                    fontWeight={"bold"}
+                    ml={2}
+                >
+                    {props.isChecked ? props.checkedMessage : props.notCheckedMessage}
+                </Text>
+            </Flex>
+        </FormControl>
+    )
+}
+
+export default CCSwitchInput
