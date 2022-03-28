ad70b437c19769eb6fc4b179e156b1bdf24e9200
Samad <eeysaad@nottingham.ac.uk>
2022-03-28 12:51:10 +0100

    renamed data tbl to values tbl

--- react-client-app/src/stories/DataTbl.stories.js
+++ react-client-app/src/stories/DataTbl.stories.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import DataTbl from '../components/DataTbl';
+import ValuesTbl from '../components/ValuesTbl';
 
 export default {
-  title: 'Components/DataTbl',
-  component: DataTbl
+  title: 'Components/ValuesTbl',
+  component: ValuesTbl
 };
 
-export const Template = (args) => <DataTbl />;
+export const Template = (args) => <ValuesTbl />;
 
 
