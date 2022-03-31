c38b88aaf76ff7a844eea5ec5efb90b01ef4e825
Sam Cox <sam.cox@nottingham.ac.uk>
2022-03-31 12:35:57 +0100

    Format list views of most remaining classes to provide more helpful information.

--- api/mapping/admin.py
+++ api/mapping/admin.py
@@ -19,6 +19,13 @@ from .models import (
 )
 
 
+class DataPartnerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+
 class ScanReportAdmin(admin.ModelAdmin):
     raw_id_fields = ("data_dictionary",)
     filter_horizontal = (
@@ -45,10 +52,63 @@ class ScanReportTableAdmin(admin.ModelAdmin):
         "person_id",
         "date_event",
     )
+    list_display = (
+        "id",
+        "get_name",
+        "get_scan_report",
+    )
+
+    def get_name(self, obj):
+        return obj.name
+
+    get_name.short_description = "Name"
+
+    def get_scan_report(self, obj):
+        return "%s: %s" % (obj.scan_report.id, obj.scan_report.dataset)
+
+    get_scan_report.short_description = "Scan Report"
+
+
+class ScanReportFieldAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "get_name",
+        "get_scan_report_table",
+        "get_scan_report"
+    )
+
+    def get_name(self, obj):
+        return obj.name
+
+    get_name.short_description = "Name"
+
+    def get_scan_report_table(self, obj):
+        return "%s: %s" % (obj.scan_report_table.id, obj.scan_report_table.name)
+
+    get_scan_report_table.short_description = "Scan Report Table"
+
+    def get_scan_report(self, obj):
+        return "%s: %s" % (obj.scan_report_table.scan_report.id,
+                           obj.scan_report_table.scan_report.dataset)
+
+    get_scan_report.short_description = "Scan Report"
 
 
 class ScanReportValueAdmin(admin.ModelAdmin):
     raw_id_fields = ("scan_report_field",)
+    list_display = (
+        "id",
+        "get_name",
+        "get_field",
+    )
+
+    def get_name(self, obj):
+        return obj.value
+    get_name.short_description = "Value"
+
+    def get_field(self, obj):
+        return "%s: %s" % (obj.scan_report_field.id, obj.scan_report_field.name)
+    get_field.short_description = "Scan Report Field"
 
 
 class MappingRuleAdmin(admin.ModelAdmin):
@@ -59,12 +119,59 @@ class MappingRuleAdmin(admin.ModelAdmin):
         "source_field",
         "concept",
     )
+    list_display = (
+        "id",
+        "concept",
+        "get_omop_field",
+        "get_concept",
+        "source_field",
+    )
+
+    def get_concept(self, obj):
+        return "%s: %s" % (obj.concept.concept.concept_id,
+                           obj.concept.concept.concept_name)
+    get_concept.short_description = "OMOP Concept"
+
+    def get_omop_field(self, obj):
+        return "%s" % (obj.omop_field.field)
+    get_omop_field.short_description = "OMOP Field"
+
+
+class DataDictionaryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+
+class OmopTableAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "table",
+    )
+
+
+class OmopFieldAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "field",
+        "get_table",
+    )
+
+    def get_table(self, obj):
+        return "%s: %s" % (obj.table.id, obj.table.table)
+    get_table.short_description = "OMOP Table"
 
 
 class ScanReportConceptAdmin(admin.ModelAdmin):
     raw_id_fields = (
         "concept",
         "content_object",
+    )
+    list_display = (
+        "id",
+        "concept",
+        "object_id",
     )
 
 
@@ -73,6 +180,11 @@ class DatasetAdmin(admin.ModelAdmin):
         "viewers",
         "admins",
         "editors",
+    )
+    list_display = (
+        "id",
+        "name",
+        "visibility",
     )
 
 
@@ -87,17 +199,17 @@ class ProjectAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(DataPartner)
+admin.site.register(DataPartner, DataPartnerAdmin)
 admin.site.register(ScanReport, ScanReportAdmin)
 admin.site.register(ScanReportTable, ScanReportTableAdmin)
-admin.site.register(ScanReportField)
+admin.site.register(ScanReportField, ScanReportFieldAdmin)
 admin.site.register(ScanReportValue, ScanReportValueAdmin)
 admin.site.register(ScanReportAssertion)
 admin.site.register(ClassificationSystem)
-admin.site.register(OmopTable)
-admin.site.register(OmopField)
+admin.site.register(OmopTable, OmopTableAdmin)
+admin.site.register(OmopField, OmopFieldAdmin)
 admin.site.register(MappingRule, MappingRuleAdmin)
-admin.site.register(DataDictionary)
+admin.site.register(DataDictionary, DataDictionaryAdmin)
 admin.site.register(NLPModel)
 admin.site.register(ScanReportConcept, ScanReportConceptAdmin)
 admin.site.register(Dataset, DatasetAdmin)
