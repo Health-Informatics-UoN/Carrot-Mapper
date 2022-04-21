538edb5adf89c865f592885bd7514a45d5392eb0
pszdl <daniel.lea@nottingham.ac.uk>
2022-04-21 17:41:01 +0100

    allow for alternative display names for breadcrumbs

--- react-client-app/src/components/CCBreadcrumbBar.jsx
+++ react-client-app/src/components/CCBreadcrumbBar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Flex, Link, Text } from "@chakra-ui/react"
 
-const CCBreadcrumbBar = ({ pathArray }) => {
+const CCBreadcrumbBar = ({ pathArray, altNames = null }) => {
     /**
      * Breadcrumb bar to assist navigation.
      * 
      * Required args:
      *  pathArray (Array[Any]): an Array forming the path to the current page.
+     *  altNames (Array[String]?): an Array of alternative names to display in
+     *      the breadcrumbs (the href won't be affected).
      */
 
     // Check for required arguments
     if (!pathArray || !Array.isArray(pathArray)) {
         throw TypeError("`pathArray` must be an array. It can be empty (`[]`).")
     }
+    if (altNames && (!Array.isArray(altNames) || altNames.length !== pathArray.length)) {
+        throw TypeError("`altNames` must be an Array of the same length as `pathArray`")
+    }
 
     const breadcrumbs = ["Home", ...pathArray]
+    const altBreadcrumbs = altNames !== null ? ["Home", ...altNames] : null
 
     return (
         <Flex>
@@ -23,7 +29,9 @@ const CCBreadcrumbBar = ({ pathArray }) => {
                     (breadcrumb, index) => {
                         return (
                             <>
-                                <Link href={`/${pathArray.slice(0, index).join("/")}`}>{breadcrumb}</Link>
+                                <Link href={`/${pathArray.slice(0, index).join("/")}`}>
+                                    {altBreadcrumbs !== null ? altBreadcrumbs[index] : breadcrumb}
+                                </Link>
                                 {index !== breadcrumbs.length - 1 &&
                                     <Text>&nbsp;/&nbsp;</Text>
                                 }
--- react-client-app/src/stories/CCBreadcrumbBar.stories.js
+++ react-client-app/src/stories/CCBreadcrumbBar.stories.js
@@ -9,20 +9,47 @@ export default {
 
 const Template = (args) => <CCBreadcrumbBar {...args} />;
 
+// Home
 export const CrumbsAtRoot = Template.bind()
 CrumbsAtRoot.args = {pathArray : []}
 
+// Home / Datasets
 export const OneCrumb = Template.bind()
 OneCrumb.args = {pathArray : ["Datasets"]}
 
+// Home / Datasets / 1234
 export const TwoCrumbs = Template.bind()
 TwoCrumbs.args = {pathArray : ["Datasets", 1234]}
 
+// Home / Datasets / 1234 / Details
 export const ThreeCrumbs = Template.bind()
 ThreeCrumbs.args = {pathArray : ["Datasets", 1234, "Details"]}
 
+// Throws TypeError
 export const UndefindedCrumbs = Template.bind()
 UndefindedCrumbs.args = {pathArray : undefined}
 
+// Throws TypeError
 export const NonArrayCrumbs = Template.bind()
 NonArrayCrumbs.args = {pathArray : "abc"}
+
+// Home / Datasets / The Fellowship of The Ring / Details
+export const AltNames = Template.bind()
+AltNames.args = {
+    pathArray : ["Datasets", 1234, "Details"],
+    altNames : ["Datasets", "The Fellowship of The Ring", "Details"]
+}
+
+// Throws TypeError
+export const AltNamesNotEqualLength = Template.bind()
+AltNamesNotEqualLength.args = {
+    pathArray : ["Datasets", 1234, "Details"],
+    altNames : ["Datasets", "The Fellowship of The Ring"]
+}
+
+// Throws TypeError
+export const AltNamesNotArray = Template.bind()
+AltNamesNotArray.args = {
+    pathArray : ["Datasets", 1234, "Details"],
+    altNames : "Not an Array"
+}
