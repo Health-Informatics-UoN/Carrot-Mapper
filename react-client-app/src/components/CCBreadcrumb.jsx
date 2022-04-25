67847bf712d07bb642d5ce9d607e86b0f433a126
pszdl <daniel.lea@nottingham.ac.uk>
2022-04-25 15:14:34 +0100

    docstring for `CCBreadcrumb`

--- react-client-app/src/components/CCBreadcrumb.jsx
+++ react-client-app/src/components/CCBreadcrumb.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Link } from "@chakra-ui/react"
 
 const CCBreadcrumb = ({ name, link }) => {
+    /**
+     * Reusable breadcrumb component. 
+     * 
+     * Required args:
+     *  name (String): the text to display for the crumb.
+     *  link (String): the link to the destination of the crumb.
+     */
     return <Link href={link}>{name}</Link>
 }
 
