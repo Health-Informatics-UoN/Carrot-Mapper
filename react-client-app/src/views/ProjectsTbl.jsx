b6f1c36a96708a2e68bd5c90b21586a53ccbe9f8
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-05-09 10:06:21 +0100

    add async so Promise works

--- react-client-app/src/views/ProjectsTbl.jsx
+++ react-client-app/src/views/ProjectsTbl.jsx
@@ -13,97 +13,85 @@ const ProjectsTbl = () => {
     const [loadingMessage, setLoadingMessage] = useState("Loading Projects")
     // const [ids, setIds] = useState();
     // const a = useRef(null);
-    useEffect(() => {
+    useEffect(async () => {
+        let res = await useGet(`/projects/?datasets`);
+        setProjects(res)
 
-        useGet(`/projects/?datasets`).then(res => {
-            setProjects(res)
-            let test_projects = res.sort((b, a) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
-            const datasetObject = {};
-            test_projects.map((project) => {
-                project.datasets.map((id) => {
-                    datasetObject[id] = true;
-                })
-            });
-            const datasetIds = chunkIds(Object.keys(datasetObject));
-            const datasetPromises = [];
-            for (let i = 0; i < datasetIds.length; i++) {
-                datasetPromises.push(useGet(`/datasets/?id__in=${datasetIds[i].join()}`));
-            }
-            let dataset = await Promise.all(datasetPromises)
-            dataset = dataset[0]
-            // dataset.forEach((element) => {
-            //     test_projects = test_projects.map((project) => {
-            //         project.datasets.map((id) => id == element.id ? { ...id, id: element } : id);
-            //     })
-
-            // })
-            // console.log(test_projects)
+        let test_projects = res.sort((b, a) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
+        const datasetObject = {};
+        test_projects.map((project) => {
+            project.datasets.map((id) => {
+                datasetObject[id] = true;
+            })
         });
+        const datasetIds = chunkIds(Object.keys(datasetObject));
+        const datasetPromises = [];
+        for (let i = 0; i < datasetIds.length; i++) {
+            datasetPromises.push(useGet(`/datasets/?id__in=${datasetIds[i].join()}`));
+        }
 
-        console.log(datasetPromises)
-        // datasetPromises.forEach((element) => {
-        //     console.log(element)
-        //     test_projects.map((project) => {
-        //         project.datasets.map((id) => {
-        //             id == element.id ? { ...id, name: element.name } : id
-        //         })
-        //     });
-        // });
-        console.log(test_projects)
+        let dataset = await Promise.all(datasetPromises)
+
+        console.log(dataset)
+        dataset.forEach(x => {
+            projects.map(project => {
+                project.datasets = project.datasets.map(d_id => d_id == x.id ? { ...d_id, d_id: x.name } : d_id)
+            })
+        })
+        setProjects(projects)
+        console.log(projects)
         setLoadingMessage(null)
-    })
-
-}, []);
-if (loadingMessage) {
-    //Render Loading State
+    }, []);
+    if (loadingMessage) {
+        //Render Loading State
+        return (
+            <div>
+                <Flex padding="30px">
+                    <Spinner />
+                    <Flex marginLeft="10px">{loadingMessage}</Flex>
+                </Flex>
+            </div>
+        )
+    }
     return (
         <div>
-            <Flex padding="30px">
-                <Spinner />
-                <Flex marginLeft="10px">{loadingMessage}</Flex>
+            <CCBreadcrumbBar>
+                <Link href={"/"}>Home</Link>
+                <Link href={"/projects"}>Projects</Link>
+            </CCBreadcrumbBar>
+            <Flex>
+                <Spacer />
             </Flex>
+
+            <Grid templateColumns="repeat(4, 1fr)" gap={6} >
+
+                <Text fontWeight={"bold"}>Project</Text>
+                <Text fontWeight={"bold"}>Datasets</Text>
+                {projects.length > 0 ?
+                    projects.map((item, index) =>
+                        <GridItem colSpan={4} w={"100%"} bg={index % 2 == 0 ? 'greyBasic.100' : 'greyBasic.500'} >
+                            <Grid templateColumns="repeat(4, 1fr)">
+                                <Text>{item.name}</Text>
+                                <GridItem colSpan={3} >
+                                    {item.datasets.map((element) =>
+                                        <Grid templateColumns="repeat(3, 1fr)">
+                                            <>
+                                                <Text><Link href={"/datasets/" + element.id}>{element.name}</Link> </Text>
+                                            </>
+
+                                        </Grid>
+                                    )}
+                                </GridItem>
+                            </Grid>
+                        </GridItem >
+                    ) :
+                    <Flex padding="30px">
+                        <Flex marginLeft="10px">No Datasets in Project</Flex>
+                    </Flex>
+                }
+            </Grid >
         </div>
-    )
-}
-return (
-    <div>
-        <CCBreadcrumbBar>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/projects"}>Projects</Link>
-        </CCBreadcrumbBar>
-        <Flex>
-            <Spacer />
-        </Flex>
-
-        <Grid templateColumns="repeat(4, 1fr)" gap={6} >
-
-            <Text fontWeight={"bold"}>Project</Text>
-            <Text fontWeight={"bold"}>Datasets</Text>
-            {projects.length > 0 ?
-                projects.map((item, index) =>
-                    <GridItem colSpan={4} w={"100%"} bg={index % 2 == 0 ? 'greyBasic.100' : 'greyBasic.500'} >
-                        <Grid templateColumns="repeat(4, 1fr)">
-                            <Text>{item.name}</Text>
-                            <GridItem colSpan={3} >
-                                {item.datasets.map((id) =>
-                                    <Grid templateColumns="repeat(3, 1fr)">
-                                        <>
-                                            <Text><Link href={"/datasets/" + id}>{id}</Link> </Text>
-                                        </>
-
-                                    </Grid>
-                                )}
-                            </GridItem>
-                        </Grid>
-                    </GridItem >
-                ) :
-                <Flex padding="30px">
-                    <Flex marginLeft="10px">No Datasets in Project</Flex>
-                </Flex>
-            }
-        </Grid >
-    </div>
-);
+    );
 
 }
 export default ProjectsTbl;
