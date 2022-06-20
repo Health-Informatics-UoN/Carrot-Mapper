4c3e088002589c308c85819a925d35370aeca2d5
Sam Cox <sam.cox@nottingham.ac.uk>
2022-06-20 12:16:28 +0100

    Correct syntax referring to data sources.

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,8 +30,8 @@ data "azurerm_resource_group" "rg" {
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku {
     tier = "Free"
     size = "F1"
@@ -40,8 +40,8 @@ resource "azurerm_app_service_plan" "appserviceplan" {
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
 resource "azurerm_app_service" "webapp" {
   name                = "webapp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
   source_control {
     repo_url           = "https://github.com/HDRUK/CaRROT-Mapper"
