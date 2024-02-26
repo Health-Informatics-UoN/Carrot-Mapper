2a3272fad5501def3399a67fb2b193a6feec1dd4
Andy Rae <hello@rae.li>
2024-02-26 11:05:32 +0000

    Add test__flatten_list

--- app/workers/test/shared_code/test_helpers.py
+++ app/workers/test/shared_code/test_helpers.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 from azure.functions import QueueMessage
@@ -8,7 +8,7 @@ from shared_code import helpers
 logger = MagicMock()
 
 
-def test_unwrap_message_body():
+def test__unwrap_message_body():
     # Arrange
     message_body = '{"test key": "test value"}'
     msg = QueueMessage(body=message_body.encode("utf-8"))
@@ -20,7 +20,7 @@ def test_unwrap_message_body():
     assert unwrapped_body == {"test key": "test value"}
 
 
-def test_extract_details():
+def test__extract_details():
     # Arrange
     message = {
         "scan_report_blob": "scan_report_blob",
@@ -40,3 +40,37 @@ def test_extract_details():
         "table_id",
     )
     assert result == expected_result
+
+
+@pytest.mark.skip
+@pytest.mark.parametrize("dequeue_count, should_raise_error", [(1, False), (2, True)])
+def test__handle_failure(dequeue_count, should_raise_error):
+    # Arrange
+    msg = QueueMessage()
+    with patch.object(msg, "dequeue_count", new=dequeue_count):
+        scan_report_id = "sample_scan_report_id"
+
+        # Act
+        if should_raise_error:
+            with pytest.raises(ValueError):
+                helpers._handle_failure(msg, scan_report_id)
+        else:
+            helpers._handle_failure(msg, scan_report_id)
+
+        # Assert
+        # Ensure ValueError is raised only when it should be
+        assert (
+            logger.info.call_args_list[-1][0][0]
+            == f"dequeue_count {str(dequeue_count)}"
+        ) == (not should_raise_error)
+
+
+def test__flatten_list():
+    # Arrange
+    nested_list = [[1, 2, 3], [4, 5], [6, 7, 8]]
+
+    # Act
+    flattened_list = helpers.flatten_list(nested_list)
+
+    # Assert
+    assert flattened_list == [1, 2, 3, 4, 5, 6, 7, 8]
