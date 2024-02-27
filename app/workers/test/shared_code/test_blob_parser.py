cf2409fb82504f31a0b15aa301c09adf70b77f03
Andy Rae <hello@rae.li>
2024-02-27 15:04:04 +0000

    Add test_process_four_item_dict

--- app/workers/test/shared_code/test_blob_parser.py
+++ app/workers/test/shared_code/test_blob_parser.py
@@ -1,4 +1,9 @@
-from shared_code.blob_parser import process_three_item_dict, remove_BOM
+import pytest
+from shared_code.blob_parser import (
+    process_four_item_dict,
+    process_three_item_dict,
+    remove_BOM,
+)
 
 
 def test_remove_BOM():
@@ -58,5 +63,57 @@ def test_process_three_item_dict():
         "table1": {"field1": "code1", "field2": "code2"},
         "table2": {"field2": "code2"},
         "table3": {"field3": "code3"},
+    }
+    assert result == expected_result
+
+
+@pytest.mark.skip()
+def test_process_four_item_dict():
+    """
+    TODO: This fails despite being based on the code example.
+    We need to clarify if the code, or example is wrong.
+    """
+    # Arrange
+    four_item_data = [
+        {
+            "csv_file_name": "table1",
+            "field_name": "field1",
+            "value": "value1",
+            "code": "code1",
+        },
+        {
+            "csv_file_name": "table1",
+            "field_name": "field2",
+            "value": "value2",
+            "code": "code2",
+        },
+        {
+            "csv_file_name": "table2",
+            "field_name": "field2",
+            "value": "value2",
+            "code": "code2",
+        },
+        {
+            "csv_file_name": "table2",
+            "field_name": "field2",
+            "value": "value3",
+            "code": "code3",
+        },
+        {
+            "csv_file_name": "table3",
+            "field_name": "field3",
+            "value": "value3",
+            "code": "code3",
+        },
+    ]
+
+    # Act
+    result = process_four_item_dict(four_item_data)
+
+    # Assert
+    expected_result = {
+        "table1": {"field1": {"value1": "code1"}, "field2": {"value2": "code2"}},
+        "table2": {"field2": {"value2": "code2", "value3": "code3"}},
+        "table3": {"field3": {"value3": "code3"}},
     }
     assert result == expected_result
