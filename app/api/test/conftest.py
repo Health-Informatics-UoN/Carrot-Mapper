7985c07c12fd4e10ac0ca502ca6abdd11d89c658
Andy Rae <hello@rae.li>
2024-03-04 20:15:09 +0000

    Move to executing sql manually

--- app/api/test/conftest.py
+++ app/api/test/conftest.py
@@ -1,11 +1,41 @@
 from typing import Generator
 
+import psycopg2
 import pytest
 from data.models.concept import Concept
 from django.conf import settings
 from django.core.management import call_command
 from django.db import connection
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2.sql import Composable
 from pytest_django import DjangoDbBlocker
+
+
+def run_sql(db: str, sql: str | bytes | Composable):
+    """
+    Executes the given SQL query on the given database.
+
+    Args:
+        db: The name of the database to connect to.
+        sql: The SQL query to execute.
+
+    Returns:
+        None
+
+    Raises:
+        psycopg2.Error: If there is an error executing the SQL.
+    """
+    conn = psycopg2.connect(
+        dbname=db,
+        user=settings.DATABASES["default"]["USER"],
+        password=settings.DATABASES["default"]["PASSWORD"],
+        host=settings.DATABASES["default"]["HOST"],
+        port=settings.DATABASES["default"]["PORT"],
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.close()
 
 
 @pytest.fixture(scope="session")
@@ -22,32 +52,24 @@ def django_db_setup(
     # Set the default db to the test one for ease.
     db_name = settings.DATABASES["default"]["TEST"]["NAME"]
 
-    # Manually create the test DB if it doesn't exist.
-    with django_db_blocker.unblock():
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s", [db_name])
-            db_exists = cursor.fetchone()
+    # Manually create the test DB + omop Schema if it doesn't exist, drop if it does.
+    run_sql("postgres", f"DROP DATABASE IF EXISTS {db_name}")
+    run_sql("postgres", f"CREATE DATABASE {db_name} TEMPLATE template0")
+    run_sql(db_name, "CREATE SCHEMA IF NOT EXISTS omop")
 
-            if not db_exists:
-                cursor.execute(f"CREATE DATABASE {db_name} TEMPLATE template0")
-
-            connection.close()
-
+    # Set the default to test for ease
     settings.DATABASES["default"]["NAME"] = db_name
 
-    # Create the omop.Concept schema + table
+    # Create the omop.Concept table
     with django_db_blocker.unblock():
-        with connection.cursor() as cursor:
-            cursor.execute("CREATE SCHEMA IF NOT EXISTS omop")
         with connection.schema_editor() as schema_editor:
             schema_editor.create_model(Concept)
 
         # run the rest of the migrations
         call_command("migrate", "--noinput")
 
-        yield
+    yield
 
-        # If not keeping the test db, drop it
-        if not django_db_keepdb:
-            with connection.cursor() as cursor:
-                cursor.execute(f"DROP DATABASE {db_name}")
+    # If not keeping the test db, drop it
+    if not django_db_keepdb:
+        run_sql("postgres", f"DROP DATABASE {db_name}")
