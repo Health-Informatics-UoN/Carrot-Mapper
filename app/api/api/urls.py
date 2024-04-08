1d9b45f9503ab0e0b88d08657da907f9c2a2993a
Andy Rae <hello@rae.li>
2024-04-08 09:41:22 +0100

    Merge django proxy

--- app/api/api/urls.py
+++ app/api/api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include("proxy.urls")),
     path("", include("mapping.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
