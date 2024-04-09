1ecf248b3d3ef57fe6d4e45672087b6623313af8
Andy Rae <hello@rae.li>
2024-04-09 15:57:56 +0100

    Add empty data library

--- app/api/api/settings.py
+++ app/api/api/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "extra_views",
     "mapping",
+    "data",
     "rest_framework",
     "django_filters",
     "rest_framework.authtoken",
