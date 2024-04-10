fa444665e4195812cb442ecac6792e790f943415
Andy Rae <hello@rae.li>
2024-04-10 13:02:39 +0100

    Add mapping rules function

---
+++ app/workers/MappingRules/__init__.py
@@ -0,0 +1,11 @@
+import azure.functions as func
+
+
+def main(msg: func.QueueMessage):
+    """
+    Creates mapping rules.
+    Args:
+        msg (func.QueueMessage): The message received from the queue.
+    """
+
+    return
