11c49ec3351f41619008d7045a1651385d7cb5fa
Andy Rae <hello@rae.li>
2024-04-11 14:57:21 +0100

    Add logging

--- app/workers/MappingRules/__init__.py
+++ app/workers/MappingRules/__init__.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import azure.functions as func
+from shared_code import logger
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shared_code.django_settings")
 import django
@@ -23,7 +24,9 @@ def main(msg: func.QueueMessage):
     """
     message_body = json.loads(msg.get_body().decode("utf-8"))
     table_id = message_body.get("table_id")
+    logger.info("We're now mapping some rules")
 
     refresh_mapping_rules(table_id)
+    logger.info("We are now refreshed.")
 
     return
