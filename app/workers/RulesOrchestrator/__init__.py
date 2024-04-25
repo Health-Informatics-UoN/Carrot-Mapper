5e4b5f92df3f5fe386d6542106264f9880b7ba80
Andy Rae <hello@rae.li>
2024-04-25 14:42:11 +0100

    Add simple durable architecture

--- app/workers/RulesOrchestrator/__init__.py
+++ app/workers/RulesOrchestrator/__init__.py
@@ -6,9 +6,9 @@ import azure.functions as func
 
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
-    result1 = yield context.call_activity("Hello", "Tokyo")
-    result2 = yield context.call_activity("Hello", "Seattle")
-    result3 = yield context.call_activity("Hello", "London")
+    result1 = yield context.call_activity("RulesActivity", "Tokyo")
+    result2 = yield context.call_activity("RulesActivity", "Seattle")
+    result3 = yield context.call_activity("RulesActivity", "London")
     return [result1, result2, result3]
 
 
---
+++ app/workers/RulesTrigger/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+import azure.durable_functions as df
+import azure.functions as func
+
+
+async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+    client = df.DurableOrchestrationClient(starter)
+    instance_id = await client.start_new(req.route_params["functionName"], None, None)
+
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+
+    return client.create_check_status_response(req, instance_id)
