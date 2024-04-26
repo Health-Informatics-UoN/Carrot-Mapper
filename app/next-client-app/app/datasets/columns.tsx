ebfc64b5f8faa0f2a66a42a69c34c92329973d9c
andrewthien <anretrithien@gmail.com>
2024-04-26 16:07:20 +0100

    Remove "Select" column

--- app/next-client-app/app/datasets/columns.tsx
+++ app/next-client-app/app/datasets/columns.tsx
@@ -9,37 +9,13 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Checkbox } from "@/components/ui/checkbox";
 import { archiveDataSets } from "@/api/datasets";
 import { EyeNoneIcon, EyeOpenIcon, Pencil2Icon } from "@radix-ui/react-icons";
 
 export const columns: ColumnDef<DataSetResult>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: true,
-    enableHiding: false,
-  },
   {
     accessorKey: "id",
     header: ({ column }) => (
