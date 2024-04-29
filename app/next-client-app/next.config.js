70e1dc694470c6c8b194e32e51a552097aca1fd5
andrewthien <anretrithien@gmail.com>
2024-04-29 13:53:49 +0100

    Modify the NextJS config

--- app/next-client-app/next.config.js
+++ app/next-client-app/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
-      allowedOrigins: [process.env.BACKEND_ORIGIN],
+      allowedOrigins: [process.env.BACKEND_URL, "127.0.0.1:8000"],
     },
   },
 };
