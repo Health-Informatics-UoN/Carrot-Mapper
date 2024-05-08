5212e8159a615a0af394a5c6fcb2a1ca5f3e4756
andrewthien <anretrithien@gmail.com>
2024-05-08 13:18:05 +0100

    Add linkPrefix to the dataset page

--- app/next-client-app/app/datasets/page.tsx
+++ app/next-client-app/app/datasets/page.tsx
@@ -67,6 +67,7 @@ export default async function DataSets({ searchParams }: DataSetListProps) {
               data={dataset.results}
               count={dataset.count}
               filter="name"
+              linkPrefix=""
             />
           </TabsContent>
           <TabsContent value="archived">
@@ -75,6 +76,7 @@ export default async function DataSets({ searchParams }: DataSetListProps) {
               data={dataset.results}
               count={dataset.count}
               filter="name"
+              linkPrefix=""
             />
           </TabsContent>
         </Tabs>
