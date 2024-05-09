bfc4a65f78c0ae10a406e1b7f47910ec9429be33
Andy Rae <hello@rae.li>
2024-05-09 17:05:10 +0100

    Add api filtering

--- app/api/api/views.py
+++ app/api/api/views.py
@@ -365,7 +365,11 @@ class ScanReportListViewSetV2(ScanReportListViewSet):
     """
 
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = {"hidden": ["exact"], "dataset": ["in", "icontains"]}
+    filterset_fields = {
+        "hidden": ["exact"],
+        "dataset": ["in", "icontains"],
+        "status": ["in"],
+    }
     ordering_fields = ["id", "name", "created_at", "dataset", "data_partner"]
     pagination_class = CustomPagination
     ordering = "-created_at"
