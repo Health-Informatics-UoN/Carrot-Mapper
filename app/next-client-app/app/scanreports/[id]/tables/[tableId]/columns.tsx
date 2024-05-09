07e9a1c5aa1bcc2938b167ab826ccaa9fe697a57
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-05-09 16:06:04 +0100

    Add form to add concept

--- app/next-client-app/app/scanreports/[id]/tables/[tableId]/columns.tsx
+++ app/next-client-app/app/scanreports/[id]/tables/[tableId]/columns.tsx
@@ -4,6 +4,9 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/data-table/DataTableColumnHeader";
+import { Input } from "@/components/ui/input";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 
 export const columns: ColumnDef<ScanReportField>[] = [
   {
@@ -55,8 +58,55 @@ export const columns: ColumnDef<ScanReportField>[] = [
     enableSorting: true,
   },
   {
+    id: "Add Concept",
+    header: "",
+    cell: ({ row }) => {
+      const validationSchema = Yup.object().shape({
+        concept: Yup.number()
+          .required("Field is required")
+          .min(1, "Add a valid number"),
+      });
+
+      const handleSubmit = async (value, actions) => {
+        actions.setSubmitting(true);
+      };
+
+      return (
+        <Formik
+          initialValues={{ concept: "" }}
+          validationSchema={validationSchema}
+          onSubmit={(data, actions) => {
+            handleSubmit(data, actions);
+            actions.resetForm();
+          }}
+        >
+          {({ values, handleChange, handleSubmit, errors, touched }) => (
+            <Form onSubmit={handleSubmit}>
+              <div className="flex gap-2 w-3/4">
+                <div className="flex-none">
+                  <Input
+                    type="number"
+                    name="concept"
+                    value={values.concept}
+                    onChange={handleChange}
+                  />
+                  {errors.concept && touched.concept ? (
+                    <p style={{ color: "red" }}>{errors.concept}</p>
+                  ) : null}
+                </div>{" "}
+                <Button type="submit">Add</Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      );
+    },
+  },
+  {
     id: "edit",
-    header: ({ column }) => <DataTableColumnHeader column={column} title="" />,
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Edit" />
+    ),
     cell: ({ row }) => {
       const { id } = row.original;
       return (
