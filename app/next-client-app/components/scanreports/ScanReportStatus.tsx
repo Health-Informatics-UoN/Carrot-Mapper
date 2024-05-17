ed0cc7f69acc7f0c9790c7a005ae79354750d121
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-05-17 11:00:57 +0100

    Fix ScanReportStatus prop

--- app/next-client-app/components/scanreports/ScanReportStatus.tsx
+++ app/next-client-app/components/scanreports/ScanReportStatus.tsx
@@ -11,8 +11,8 @@ import { ApiError } from "@/lib/api/error";
 import { Row } from "@tanstack/react-table";
 import { toast } from "sonner";
 
-export function ScanReportStatus({ row }: { row: Row<ScanReportResult> }) {
-  const { id, status, dataset } = row.original as ScanReportList;
+export function ScanReportStatus({ row }: { row: Row<ScanReportList> }) {
+  const { id, status, dataset } = row.original;
   // Safely extract the color
   const statusInfo = statusOptions.find((option) => option.value === status);
   const textColorClassName = statusInfo?.color ?? "text-black";
