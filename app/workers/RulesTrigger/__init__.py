e1b4834dbdd31c27bb4933fe819d0b694a9bcda1
Andy Rae <hello@rae.li>
2024-05-19 18:55:37 +0100

    Update rules trigger

--- app/workers/RulesTrigger/__init__.py
+++ app/workers/RulesTrigger/__init__.py
@@ -1,16 +1,28 @@
+import json
 import logging
+import uuid
 
 import azure.durable_functions as df  # type: ignore
 import azure.functions as func
 
 
-async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
-    client = df.DurableOrchestrationClient(starter)
+async def main(
+    req: func.HttpRequest, msg: func.Out[func.QueueMessage], starter: str
+) -> func.HttpResponse:
 
-    msg = req.get_json()
+    msg_body = req.get_json()
 
-    instance_id = await client.start_new(req.route_params["functionName"], None, msg)
+    # Create a unique instance ID
+    instance_id = str(uuid.uuid4())
+    msg_body["instance_id"] = instance_id
 
-    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+    msg.set(json.dumps(msg_body))
 
-    return client.create_check_status_response(req, instance_id)
+    logging.info(f"Queued message with instance ID = '{instance_id}'.")
+
+    status_url = f"{req.url.rstrip('/')}/status/{instance_id}"
+    return func.HttpResponse(
+        json.dumps({"instanceId": instance_id, "statusQueryGetUri": status_url}),
+        status_code=202,
+        mimetype="application/json",
+    )
