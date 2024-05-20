4dc31d59b4a4b5a9ed30b88ee29a2fe912c403f5
Andy Rae <hello@rae.li>
2024-05-20 19:23:13 +0100

    Remove rule creation

--- app/next-client-app/app/scanreports/[id]/tables/[tableId]/add-concept.tsx
+++ app/next-client-app/app/scanreports/[id]/tables/[tableId]/add-concept.tsx
@@ -33,7 +33,6 @@ export default function AddConcept({ id, tableId }: AddConceptProps) {
 
   const handleSubmit = async (conceptCode: number) => {
     try {
-      // create scan report concept
       await addConcept({
         concept: conceptCode,
         object_id: id,
@@ -42,38 +41,6 @@ export default function AddConcept({ id, tableId }: AddConceptProps) {
         table_id: tableId,
       });
       toast.success("ConceptId linked to the value");
-
-      // let scanreportconcepts = await getScanReportConcepts(objectQuery);
-      // if (scanreportconcepts.length > 0) {
-      // if (false) {
-      //   const conceptIds = scanreportconcepts.map((value) => value.concept);
-      //   const conceptFilters = await getConceptFilters(conceptIds.join());
-
-      //   // save new concepts to state
-      //   const scanreport_concepts = scanreportconcepts.map((element) => ({
-      //     ...element,
-      //     concept: conceptFilters.find(
-      //       (con) => con.concept_id == element.concept
-      //     ),
-      //   }));
-
-      //   // create mapping rules for new concept
-      //   const scan_report_concept = scanreport_concepts.filter(
-      //     (con) => con.concept?.concept_id == conceptCode
-      //   )[0];
-      //   try {
-      //     await saveMappingRules(
-      //       scan_report_concept as any,
-      //       table,
-      //       destination_field
-      //     );
-      //     toast.success("Mapping Rules created");
-      //   } catch (error) {
-      //     handleError(error, "Could not create mapping rules");
-      //   }
-      // } else {
-      //   toast.error("Could not find the concepts");
-      // }
     } catch (error) {
       handleError(error, "Adding concept failed!");
     }
