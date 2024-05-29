f50f2732cd7e3a211be3a24edc478dca6bb30c79
andrewthien <anretrithien@gmail.com>
2024-05-29 12:18:46 +0100

    Add the Alert Box

--- app/next-client-app/app/scanreports/[id]/tables/[tableId]/update/page.tsx
+++ app/next-client-app/app/scanreports/[id]/tables/[tableId]/update/page.tsx
@@ -18,6 +18,8 @@ import { FilterParameters } from "@/types/filter";
 import { getConceptFilters, getScanReportConcepts } from "@/api/concepts";
 import { addConceptsToResults } from "@/lib/concept-utils";
 import { ButtonsRow } from "@/components/scanreports/ButtonsRow";
+import { AlertCircleIcon } from "lucide-react";
+import { Alert } from "@/components/ui/alert";
 
 interface ScanReportsFieldProps {
   params: {
@@ -95,7 +97,20 @@ export default async function ScanReportsValue({
       <div className="mt-3">
         <h1 className="text-4xl font-semibold">Update Table</h1>
       </div>
-      <ButtonsRow scanreportId={id} tableId={tableId} />
+      <Alert className="flex items-center gap-3 bg-carrot-600 text-white mt-4">
+        <div>
+          <AlertCircleIcon />
+        </div>
+        <div>
+          {" "}
+          Mapping Rules cannot be generated without the Person ID and Date Event
+          being set.
+          <br />
+          Once you set these, Mapping Rules will be generated for all Concepts
+          currently associated to the table.
+          <br />
+        </div>
+      </Alert>
     </div>
   );
 }
