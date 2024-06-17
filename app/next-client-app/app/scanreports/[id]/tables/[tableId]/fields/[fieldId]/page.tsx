1dcb3a12d10babc0a634e695592d1dd2bc748a5d
andrewthien <anretrithien@gmail.com>
2024-06-17 10:41:35 +0100

    test

--- app/next-client-app/app/scanreports/[id]/tables/[tableId]/fields/[fieldId]/page.tsx
+++ app/next-client-app/app/scanreports/[id]/tables/[tableId]/fields/[fieldId]/page.tsx
@@ -34,7 +34,7 @@ export default async function ScanReportsValue({
   params: { id, tableId, fieldId },
   searchParams,
 }: ScanReportsValueProps) {
-  const defaultPageSize = 30;
+  const defaultPageSize = 20;
   const defaultParams = {
     scan_report_field: fieldId,
     page_size: defaultPageSize,
