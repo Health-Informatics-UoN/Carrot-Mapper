b1816b2c33b9861bc598f69bab2a7d092184bcfa
Andy Rae <hello@rae.li>
2024-06-22 16:16:35 +0100

    Fix utils

--- app/next-client-app/lib/api/utils.ts
+++ app/next-client-app/lib/api/utils.ts
@@ -1,23 +1,25 @@
 import request from "@/lib/api/request";
 
 /**
- * Fetches all pages of paginated data from the specified initial URL.
+ * Fetches all pages of paginated data from the specified url.
  *
  * Iterates through paginated data until all pages are fetched and concatenated.
  *
  * @template T - The type of data to be fetched
- * @param {string} initialUrl - The initial URL to start fetching paginated data from
+ * @param {string} url - The url to fetch from
  * @returns {Promise<T[]>} - A promise that resolves to an array of all fetched data
  */
-export async function fetchAllPages<T>(initialUrl: string): Promise<T[]> {
-  let url: string | null = initialUrl;
+export async function fetchAllPages<T>(url: string): Promise<T[]> {
   let allResults: T[] = [];
+  let pageNumber = 1;
 
-  while (url) {
-    const data: PaginatedResponse<T> = await request(url);
+  while (true) {
+    const urlWithPage = `${url}&p=${pageNumber}`;
+    const data: PaginatedResponse<T> = await request(urlWithPage);
 
     allResults = allResults.concat(data.results);
-    url = data.next;
+    pageNumber++;
+    if (!data.next) break;
   }
 
   return allResults;
