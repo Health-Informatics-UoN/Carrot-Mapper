391202ce8d3e4eabd92e1f0377c7222308f86efb
Andy Rae <hello@rae.li>
2024-06-22 15:46:27 +0100

    Revert max_page_size

--- app/api/api/paginations.py
+++ app/api/api/paginations.py
@@ -4,5 +4,5 @@ from rest_framework import pagination
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
-    max_page_size = 100
+    max_page_size = 50
     page_query_param = "p"
