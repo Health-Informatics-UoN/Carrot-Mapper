358eb8c4379473ebc5ed42ce4d4067077712d98d
andrewthien <anretrithien@gmail.com>
2024-07-25 13:50:23 +0100

    Change layout

--- app/next-client-app/app/layout.tsx
+++ app/next-client-app/app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        {/* <Navbar />
         <div className="mt-[50px] mb-4">{children}</div>
-        <BootstrapClient />
-        {/* <Sidebar /> */}
-        {/* <div className="ml-[230px] mb-4">{children}</div> */}
+        <BootstrapClient /> */}
+        <Sidebar />
+        <div className="ml-[230px] mb-4">{children}</div>
 
         <Toaster
           toastOptions={{
