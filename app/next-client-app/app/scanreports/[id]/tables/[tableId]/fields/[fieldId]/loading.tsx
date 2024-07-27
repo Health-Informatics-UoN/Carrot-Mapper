0506eaf2dc95ebeace7c7baaf48928d053eae0e7
Andy Rae <hello@rae.li>
2024-07-27 16:14:34 +0100

    Loading at value level

---
+++ app/next-client-app/app/scanreports/[id]/tables/[tableId]/fields/[fieldId]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4"></div>
+    </div>
+  );
+}
