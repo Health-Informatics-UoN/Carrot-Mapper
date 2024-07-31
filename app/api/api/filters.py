ce5456775452a16b660f6c2ce4df246f2a8bcab4
andrewthien <anretrithien@gmail.com>
2024-07-31 10:00:13 +0100

    revert V1 filter

--- app/api/api/filters.py
+++ app/api/api/filters.py
@@ -5,6 +5,11 @@ import os
 
 
 class ScanReportAccessFilter(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see Scan Reports they are allowed to view, edit, or admin.
+    """
+
+    # Each model type needs a different relationship to get the Scan Report / Dataset permissions.
     RELATIONSHIP_MAPPING = {
         "scanreport": "",
         "scanreporttable": "scan_report__",
@@ -13,59 +18,69 @@ class ScanReportAccessFilter(filters.BaseFilterBackend):
     }
 
     def filter_queryset(self, request, queryset, view):
-        user = request.user
-
-        print(f"Filtering for user: {user.username}")
-
-        if user.username == os.getenv("AZ_FUNCTION_USER"):
-            print("AZ_FUNCTION_USER detected, returning full queryset")
-            return queryset
-
         model = queryset.model.__name__.lower()
         relationship = self.RELATIONSHIP_MAPPING.get(model, "")
-        print(f"Model: {model}, Relationship: {relationship}")
-        print(f"RELATIONSHIP_MAPPING keys: {self.RELATIONSHIP_MAPPING.keys()}")
-
-        relationship = self.RELATIONSHIP_MAPPING.get(model, "")
-        print(f"Relationship: '{relationship}'")
+        print(model, relationship)
+        user_id = request.user.id
 
         visibility_conditions = self.get_visibility_conditions(relationship)
-        permission_conditions = self.get_permission_conditions(relationship, user.id)
-
-        print("Visibility conditions:", visibility_conditions)
-        print("Permission conditions:", permission_conditions)
-
-        filtered_queryset = (
-            queryset.filter((visibility_conditions & permission_conditions))
-            .filter(**{f"{relationship}parent_dataset__project__members": user.id})
-            .distinct()
+        permission_conditions = self.get_permission_conditions(relationship, user_id)
+        print(visibility_conditions, permission_conditions)
+        print(
+            queryset.filter((visibility_conditions & permission_conditions)).distinct()
         )
-
-        return filtered_queryset
+        return queryset.filter(
+            (visibility_conditions & permission_conditions)
+        ).distinct()
 
     def get_visibility_conditions(self, relationship: str) -> Q:
+        """
+        Get visibility conditions for a given relationship.
+
+        Args:
+            relationship (str): The relationship for which visibility conditions are needed.
+
+        Returns:
+            Q: Query object representing the visibility conditions.
+        """
         dataset_visibility = f"{relationship}parent_dataset__visibility"
         scan_report_visibility = f"{relationship}visibility"
 
-        return (
+        return Q(
             Q(**{dataset_visibility: VisibilityChoices.PUBLIC})
             | Q(**{dataset_visibility: VisibilityChoices.RESTRICTED})
-        ) & (
+        ) & Q(
             Q(**{scan_report_visibility: VisibilityChoices.PUBLIC})
             | Q(**{scan_report_visibility: VisibilityChoices.RESTRICTED})
         )
 
-    def get_permission_conditions(self, relationship: str, user_id: int) -> Q:
+    def get_permission_conditions(self, relationship: str, user_id: str) -> Q:
+        """
+        Get permission conditions for a given relationship and user.
+
+        Args:
+            relationship (str): The relationship for which permission conditions are needed.
+            user_id (str): The user ID for which permission conditions are generated.
+
+        Returns:
+            Q: Query object representing the permission conditions.
+        """
+        scan_report_viewers = f"{relationship}viewers"
+        scan_report_editors = f"{relationship}editors"
+        scan_report_author = f"{relationship}author"
+        dataset_viewers = f"{relationship}parent_dataset__viewers"
+        dataset_editors = f"{relationship}parent_dataset__editors"
+        dataset_admins = f"{relationship}parent_dataset__admins"
+        project_members = f"{relationship}parent_dataset__project__members"
+
         return Q(
-            Q(**{f"{relationship}viewers": user_id})
-            | Q(**{f"{relationship}editors": user_id})
-            | Q(**{f"{relationship}author": user_id})
-            | Q(**{f"{relationship}parent_dataset__viewers": user_id})
-            | Q(**{f"{relationship}parent_dataset__editors": user_id})
-            | Q(**{f"{relationship}parent_dataset__admins": user_id})
-        ) | (
-            Q(**{f"{relationship}parent_dataset__visibility": VisibilityChoices.PUBLIC})
-            & Q(**{f"{relationship}visibility": VisibilityChoices.PUBLIC})
+            Q(**{scan_report_viewers: user_id})
+            | Q(**{scan_report_editors: user_id})
+            | Q(**{scan_report_author: user_id})
+            | Q(**{dataset_viewers: user_id})
+            | Q(**{dataset_editors: user_id})
+            | Q(**{dataset_admins: user_id})
+            | Q(**{project_members: user_id})
         )
 
 
