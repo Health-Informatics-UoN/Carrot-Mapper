35c1fbbb9ae0efccf67301caf3b4e447261781a3
Andy Rae <hello@rae.li>
2024-08-04 16:10:45 +0100

    Proxy everything

--- app/api/config/urls.py
+++ app/api/config/urls.py
@@ -1,19 +1,20 @@
 import os
 
+from config import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from revproxy.views import ProxyView
 
 urlpatterns = [
-    (
-        path("", include("proxy.urls"))
-        if os.environ.get("ENABLE_PROXY", "False").lower() == "true"
-        else None
-    ),
     path("api/", include("api.urls")),
-    path("", include("mapping.urls")),
     path("api_auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    (
+        re_path(r"(?P<path>.*)", ProxyView.as_view(upstream=f"{settings.NEXTJS_URL}/"))
+        if os.environ.get("ENABLE_PROXY", "False").lower() == "true"
+        else None
+    ),
 ]
 
 urlpatterns = [url for url in urlpatterns if url is not None]
