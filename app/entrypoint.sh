a2ab90f599a71cb6749ce1d305d6a1dccfa33a7f
andrewthien <anretrithien@gmail.com>
2024-08-07 12:09:58 +0100

    Cleaning up

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -10,29 +10,6 @@ wait-for-it ${COCONNECT_DB_HOST}:${COCONNECT_DB_PORT} -- echo "Database is ready
 
 cd /api
 
-echo "Loading mapping data"
-python manage.py loaddata mapping
-if [ $? -ne 0 ]; then
-    echo "Error loading mapping data"
-    exit 1
-fi
-
-# Check if a superuser named 'admin' exists
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); exit(0) if User.objects.filter(username='admin', is_superuser=True).exists() else exit(1)"
-if [ $? -eq 0 ]; then
-  echo "Superuser already exists. Skipping creating superuser..."
-else
-  echo "Creating superuser..."
-  python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-  if [ $? -ne 0 ]; then
-    echo "Error creating superuser"
-    exit 1
-  fi
-
-  # Set superuser password
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | python manage.py shell
-fi
-
 # Collect static files for serving
 rm -rf staticfiles
 mkdir staticfiles
