4341dcb3c75edf7b15d2749977296e98c46c9152
andrewthien <anretrithien@gmail.com>
2024-08-12 15:54:01 +0100

    adjust pic's height

--- app/next-client-app/app/(public)/page.tsx
+++ app/next-client-app/app/(public)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Default() {
   return (
     <>
-      <div className="relative w-[600x] h-[530px] ">
+      <div className="relative w-[600x] h-[1000px] ">
         <Image src="/logos/demo.jpeg" alt="demo" fill />
       </div>
     </>
