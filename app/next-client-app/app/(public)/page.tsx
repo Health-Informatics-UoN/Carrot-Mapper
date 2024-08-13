f874af81aa634d6ee43b6766db402b5cdd286132
andrewthien <anretrithien@gmail.com>
2024-08-13 09:30:04 +0100

    revert back some features and clean up

--- app/next-client-app/app/(public)/page.tsx
+++ app/next-client-app/app/(public)/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Default() {
-  return (
-    <>
-      <div className="relative w-[600x] h-[1000px] ">
-        <Image src="/logos/demo.jpeg" alt="demo" fill />
-      </div>
-    </>
-  );
+  redirect("/scanreports");
 }
