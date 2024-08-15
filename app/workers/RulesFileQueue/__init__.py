a11b7e415872ea97c529dc33978220c4ca49834a
Andy Rae <hello@rae.li>
2024-08-15 17:02:25 +0100

    Refactor file handling

--- app/workers/RulesFileQueue/__init__.py
+++ app/workers/RulesFileQueue/__init__.py
@@ -1,11 +1,19 @@
 import io
 import json
+import os
 from datetime import datetime
 from io import BytesIO
-from typing import Any, Dict, Literal, TypedDict
+from typing import Dict
 
 import azure.functions as func
-from django.db.models.query import QuerySet
+from shared_code.models import FileHandlerConfig, RulesFileMessage
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shared_code.django_settings")
+import django
+
+django.setup()
+
+from django.db.models.query import QuerySet  # type: ignore
 from shared.data.models import MappingRule, ScanReport
 from shared.files.models import FileDownload, FileType
 from shared.files.service import upload_blob
@@ -16,21 +24,17 @@ from shared.services.rules_export import (
 )
 
 
-def create_json_rules(rules: QuerySet[MappingRule]):
+def create_json_rules(rules: QuerySet[MappingRule]) -> BytesIO:
     data = get_mapping_rules_json(rules)
     json_data = json.dumps(data)
-    return BytesIO(json_data.encode("utf-8"))
+    json_bytes = BytesIO(json_data.encode("utf-8"))
+    json_bytes.seek(0)
+    return json_bytes
 
 
-def create_csv_rules(rules: QuerySet[MappingRule]):
+def create_csv_rules(rules: QuerySet[MappingRule]) -> BytesIO:
     data = get_mapping_rules_as_csv(rules)
     return io.BytesIO(data.getvalue().encode("utf-8"))
-
-
-class MessageBody(TypedDict):
-    scan_report_id: int
-    user_id: str
-    file_type: Literal["text/csv", "application/json", "image/svg+xml"]
 
 
 def main(msg: func.QueueMessage) -> None:
@@ -46,44 +50,43 @@ def main(msg: func.QueueMessage) -> None:
         msg (func.QueueMessage): The message received from the queue.
     """
     # Unwrap message
-    msg_body: MessageBody = json.loads(msg.get_body().decode("utf-8"))
+    msg_body: RulesFileMessage = json.loads(msg.get_body().decode("utf-8"))
     scan_report_id = msg_body.get("scan_report_id")
     user_id = msg_body.get("user_id")
     file_type = msg_body.get("file_type")
 
-    # Get models for this Scan Report
-    scan_report = ScanReport.objects.get(scan_report_id)
+    # Get models for this SR
+    scan_report = ScanReport.objects.get(id=scan_report_id)
     rules = MappingRule.objects.filter(scan_report__id=scan_report_id).all()
 
-    # Generate the file
-    file_handlers: Dict[str, Dict[str, Any]] = {
-        "text/csv": {
-            "handler": lambda rules: create_csv_rules(rules),
-            "file_type_value": "mapping_csv",
-        },
-        "application/json": {
-            "handler": lambda rules: create_json_rules(rules),
-            "file_type_value": "mapping_json",
-        },
-        "image/svg+xml": {
-            "handler": lambda rules: make_dag(get_mapping_rules_json(rules)),
-            "file_type_value": "mapping_svg",
-        },
+    # Setup file config
+    file_handlers: Dict[str, FileHandlerConfig] = {
+        "text/csv": FileHandlerConfig(
+            lambda rules: create_csv_rules(rules), "mapping_csv", "csv"
+        ),
+        "application/json": FileHandlerConfig(
+            lambda rules: create_json_rules(rules), "mapping_json", "json"
+        ),
+        "image/svg+xml": FileHandlerConfig(
+            lambda rules: make_dag(get_mapping_rules_json(rules)), "mapping_svg", "svg"
+        ),
     }
 
     if file_type not in file_handlers:
         raise ValueError(f"Unsupported file type: {file_type}")
 
-    # Generate the file
-    handler = file_handlers[file_type]["handler"]
-    file = handler(rules)
+    config = file_handlers[file_type]
+
+    # Generate it
+    file = config.handler(rules)
+    file_type_value = config.file_type_value
+    file_extension = config.file_extension
 
     # Save to blob
-    filename = f"Rules - {scan_report.name} - {scan_report_id} - {datetime.now()}"
-    upload_blob(filename, "rules-export", file, file_type)
+    filename = f"Rules - {scan_report.dataset} - {scan_report_id} - {datetime.now()}.{file_extension}"
+    upload_blob(filename, "rules-exports", file, file_type)
 
     # create entity
-    file_type_value = file_handlers[file_type]["file_type_value"]
     file_type_entity = FileType.objects.get(value=file_type_value)
     file_entity = FileDownload.objects.create(
         name=filename,
