30b3459833d1fb05debc8512ab2ee71adaa26f63
andrewthien <anretrithien@gmail.com>
2024-09-04 13:16:20 +0100

    Tidy up and re-organise components for landing page

--- app/next-client-app/app/(public)/page.tsx
+++ app/next-client-app/app/(public)/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from "next/navigation";
+import CallToAction from "@/components/homepage/CTA";
+import BentoProjects from "@/components/homepage/bentoProjects";
+import Features from "@/components/homepage/features";
+import Funders from "@/components/homepage/funders";
+import Hero from "@/components/homepage/hero";
 
 export default function Default() {
-  redirect("/scanreports");
+  return (
+    <div className="space-y-12 lg:space-y-28">
+      <Hero />
+      <Features />
+      <BentoProjects />
+      <Funders />
+      <CallToAction />
+    </div>
+  );
 }
