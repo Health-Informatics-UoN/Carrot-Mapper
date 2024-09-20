29f123c56b80f9885bdd65cf44b41fd451d58dd7
Andy Rae <hello@rae.li>
2024-09-20 21:46:49 +0100

    Add authn app

---
+++ app/api/authn/urls.py
@@ -0,0 +1,16 @@
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+from django.urls import path
+from django.views.decorators.csrf import get_token
+from rest_framework_simplejwt.views import TokenVerifyView
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="rest_register"),
+    path("login/", LoginView.as_view(), name="rest_login"),
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
+    path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
+    path("csrf-token/", get_token, name="api-csrf-token"),
+]
