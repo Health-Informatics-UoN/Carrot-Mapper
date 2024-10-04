99c9ede0526ab81727517a2f0143b14fca18fcba
andrewthien <anretrithien@gmail.com>
2024-10-04 11:54:18 +0100

    Add UI option 2

---
+++ app/next-client-app/app/(protected)/scanreports/[id]/columns2.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/data-table/DataTableColumnHeader";
+import { EditButton } from "@/components/scanreports/EditButton";
+import { Switch } from "@/components/ui/switch";
+import { Tooltips } from "@/components/Tooltips";
+
+export const columns2: ColumnDef<ScanReportTable>[] = [
+  {
+    id: "Name",
+    accessorKey: "name",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Name" sortName="name" />
+    ),
+    enableHiding: true,
+    enableSorting: true,
+  },
+  {
+    id: "Person ID",
+    accessorKey: "person_id",
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Person ID"
+        sortName="person_id"
+      />
+    ),
+    cell: ({ row }) => {
+      const { person_id } = row.original;
+      return <>{person_id?.name}</>;
+    },
+    enableHiding: true,
+    enableSorting: false,
+  },
+  {
+    id: "Event Date",
+    accessorKey: "date_event",
+    header: ({ column }) => (
+      <DataTableColumnHeader
+        column={column}
+        title="Date Event"
+        sortName="date_event"
+      />
+    ),
+    cell: ({ row }) => {
+      const { date_event } = row.original;
+      return <>{date_event?.name}</>;
+    },
+    enableHiding: true,
+    enableSorting: false,
+  },
+  {
+    id: "note",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Note" />
+    ),
+    cell: ({ row }) => {
+      const { id, death_table } = row.original;
+      return (
+        <div>
+          {death_table && (
+            <h3 className="flex">
+              {" "}
+              Death table
+              <Tooltips content="All of the concepts added to this table will be mapped to DEATH table" />
+            </h3>
+          )}
+        </div>
+      );
+    },
+  },
+  {
+    id: "edit",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="" />,
+    cell: ({ row }) => {
+      const { id, scan_report, permissions } = row.original;
+      return (
+        <EditButton
+          scanreportId={scan_report}
+          tableId={id}
+          type="table"
+          permissions={permissions}
+        />
+      );
+    },
+  },
+];
--- app/next-client-app/app/(protected)/scanreports/[id]/page.tsx
+++ app/next-client-app/app/(protected)/scanreports/[id]/page.tsx
@@ -8,6 +8,7 @@ import { objToQuery } from "@/lib/client-utils";
 import { FilterParameters } from "@/types/filter";
 import { DataTableFilter } from "@/components/data-table/DataTableFilter";
 import { DataTableUpdate } from "../../../../components/concepts/DataTable";
+import { columns2 } from "./columns2";
 
 interface ScanReportsTableProps {
   params: {
@@ -27,11 +28,16 @@ export default async function ScanReportsTable({
 
   const scanReportsTables = await getScanReportTables(id, query);
   const permissions = await getScanReportPermissions(id);
+  const scanReportsResult = scanReportsTables.results.map((table) => {
+    table.permissions = permissions.permissions;
+    return table;
+  });
+  const filter = <DataTableFilter filter="name" />;
 
   return (
     <div>
       <div>
-        <DataTableUpdate
+        {/* <DataTableUpdate
           count={scanReportsTables.count}
           scanReportsData={scanReportsTables.results}
           columns={columns}
@@ -39,6 +45,13 @@ export default async function ScanReportsTable({
           filterText="name "
           linkPrefix="tables/"
           permissions={permissions}
+        /> */}
+        <DataTable
+          columns={columns2}
+          data={scanReportsResult}
+          count={scanReportsTables.count}
+          Filter={filter}
+          linkPrefix="tables/"
         />
       </div>
     </div>
