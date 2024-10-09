3a45e308dc407d395e772859a52ad52f814694d7
andrewthien <anretrithien@gmail.com>
2024-10-09 14:59:04 +0100

    add tooltip to SR upload status

--- app/next-client-app/components/scanreports/UploadStatus.tsx
+++ app/next-client-app/components/scanreports/UploadStatus.tsx
@@ -3,6 +3,7 @@
 import { UploadStatusOptions } from "@/constants/scanReportStatus";
 import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { Tooltip } from "react-tooltip";
 
 interface ScanReportStatusProps {
   upload_status: string;
@@ -19,30 +20,21 @@ export function UploadStatus({ upload_status }: ScanReportStatusProps) {
   const Icon = statusInfo?.icon;
 
   return (
-    // <Select
-    //   value={upload_status}
-    //   onValueChange={handleChangeStatus}
-    //   disabled={disabled}
-    // >
-    //   <SelectTrigger className={cn(textColorClassName, className)}>
-    //     <SelectValue />
-    //   </SelectTrigger>
-    //   <SelectContent>
-    //     {UploadStatusOptions.map((option) => (
-    //       <SelectItem key={option.value} value={option.value}>
-    //         {option.label}
-    //       </SelectItem>
-    //     ))}
-    //   </SelectContent>
-    // </Select>
     <div className={cn(textColorClassName)}>
-      {Icon ? (
-        Icon === Loader2 ? (
-          <Icon className="ml-2 size-4 animate-spin" />
-        ) : (
-          <Icon className="ml-2 size-4" />
-        )
-      ) : null}
+      <a
+        data-tooltip-id="icon-tooltip"
+        data-tooltip-content={`${statusInfo?.label}`}
+        data-tooltip-place="top"
+      >
+        <Tooltip id="icon-tooltip" />
+        {Icon ? (
+          Icon === Loader2 ? (
+            <Icon className="ml-2 size-4 animate-spin" />
+          ) : (
+            <Icon className="ml-2 size-4" />
+          )
+        ) : null}
+      </a>
     </div>
   );
 }
