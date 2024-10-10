ed2c0ad34a67c0355b611043ec84f787becc76f9
andrewthien <anretrithien@gmail.com>
2024-10-10 11:41:01 +0100

    simplify UploadStatus

--- app/next-client-app/components/scanreports/UploadStatus.tsx
+++ app/next-client-app/components/scanreports/UploadStatus.tsx
@@ -5,36 +5,32 @@ import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { Tooltip } from "react-tooltip";
 
-interface ScanReportStatusProps {
-  upload_status: string;
-}
-
-export function UploadStatus({ upload_status }: ScanReportStatusProps) {
-  // Safely extract the color
+export function UploadStatus({ upload_status }: { upload_status: string }) {
   const statusInfo = UploadStatusOptions.find(
     (option) => option.value === upload_status
   );
 
-  const textColorClassName = statusInfo?.color ?? "text-black";
-
   const Icon = statusInfo?.icon;
 
+  if (!Icon) {
+    return null;
+  }
+
   return (
-    <div className={cn(textColorClassName)}>
-      <a
-        data-tooltip-id="icon-tooltip"
-        data-tooltip-content={`${statusInfo?.label}`}
-        data-tooltip-place="top"
-      >
-        <Tooltip id="icon-tooltip" />
-        {Icon ? (
-          Icon === Loader2 ? (
-            <Icon className="ml-2 size-4 animate-spin" />
-          ) : (
-            <Icon className="ml-2 size-4" />
-          )
-        ) : null}
-      </a>
-    </div>
+    <a
+      data-tooltip-id="icon-tooltip"
+      data-tooltip-content={`${statusInfo?.label}`}
+      data-tooltip-place="top"
+      className="flex justify-center"
+    >
+      <Tooltip id="icon-tooltip" />
+      <Icon
+        className={cn(
+          statusInfo.color,
+          "size-4",
+          Icon === Loader2 && "animate-spin"
+        )}
+      />
+    </a>
   );
 }
