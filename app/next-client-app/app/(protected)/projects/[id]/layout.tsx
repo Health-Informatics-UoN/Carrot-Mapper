b39b1bc8a820a9c226543c70bb35e61336ebf3b2
andrewthien <anretrithien@gmail.com>
2024-10-10 16:43:08 +0100

    add detail to layout

--- app/next-client-app/app/(protected)/projects/[id]/layout.tsx
+++ app/next-client-app/app/(protected)/projects/[id]/layout.tsx
@@ -1,6 +1,6 @@
 import { Forbidden } from "@/components/core/Forbidden";
 import { NavGroup } from "@/components/core/nav-group";
-import { Folders, Projector } from "lucide-react";
+import { BriefcaseBusiness } from "lucide-react";
 import { Boundary } from "@/components/core/boundary";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -26,7 +26,7 @@ export default async function DatasetLayout({
       name: "Datasets",
       iconName: "Folders",
     },
-    // { name: "Edit Details", slug: "details", iconName: "Edit" },
+    { name: "Edit Details", slug: "details", iconName: "Edit" },
   ];
 
   const project = await getproject(params.id);
@@ -34,6 +34,7 @@ export default async function DatasetLayout({
   // const dataPartner = dataset.data_partner;
 
   const createdDate = new Date(project.created_at);
+  const updatedDate = new Date(project.updated_at);
   // Checking permissions
   // if (
   //   !requiredPermissions.some((permission) =>
@@ -45,39 +46,43 @@ export default async function DatasetLayout({
   return (
     <div className="space-y-2">
       <div className="flex font-semibold text-xl items-center space-x-2">
-        <Projector className="text-gray-500" />
+        <BriefcaseBusiness className="text-gray-500" />
         <Link href={`/projects`}>
           <h2 className="text-gray-500 dark:text-gray-400">Projects</h2>
         </Link>
         <h2 className="text-gray-500 dark:text-gray-400">{"/"}</h2>
-        <Projector className="text-blue-700" />
+        <BriefcaseBusiness className="text-orange-700" />
         <h2>{project.name}</h2>
       </div>
 
       <div className="flex flex-col md:flex-row md:items-center text-sm space-y-2 md:space-y-0 divide-y md:divide-y-0 md:divide-x divide-gray-300">
-        {/* <h3 className="text-gray-500 dark:text-gray-400 flex items-center gap-2 pr-2">
-          <div>Project(s): </div>
-          <div className="flex space-x-1">
-            {projects.map((project) => (
-              <Badge
-                variant={"outline"}
-                className="bg-carrot-100 dark:bg-carrot-700"
-                key={project.id}
-              >
-                {project.name}
-              </Badge>
-            ))}
-          </div>
-        </h3> */}
-        {/* <InfoItem
-          label="Data Partner"
-          value={dataPartner.name}
-          className="py-1 md:py-0 md:px-3"
-        /> */}
+        <InfoItem
+          label="Members number"
+          value={project.members.length.toString()}
+          className="py-1 md:py-0 md:pr-3"
+        />
         <InfoItem
           label="Created"
           value={format(createdDate, "MMM dd, yyyy h:mm a")}
           className="py-1 md:py-0 md:px-3"
+        />
+        <InfoItem
+          label="Updated"
+          value={format(updatedDate, "MMM dd, yyyy h:mm a")}
+          className="py-1 md:py-0 md:px-3"
+        />
+      </div>
+      {/* "Navs" group */}
+      <div className="flex justify-between">
+        <NavGroup
+          path={`/projects/${params.id}`}
+          items={[
+            ...items.map((x) => ({
+              text: x.name,
+              slug: x.slug,
+              iconName: x.iconName,
+            })),
+          ]}
         />
       </div>
       <Boundary>
