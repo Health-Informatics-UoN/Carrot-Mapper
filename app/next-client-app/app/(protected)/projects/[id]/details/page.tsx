c515bfc5b275570e8c1f4cbdb2e4fba99ef99e99
andrewthien <anretrithien@gmail.com>
2024-10-10 15:41:16 +0100

    initial project detail page

---
+++ app/next-client-app/app/(protected)/projects/[id]/details/page.tsx
@@ -0,0 +1,33 @@
+import {
+  getDataPartners,
+  getDataSet,
+  getDataUsers,
+  getDatasetPermissions,
+} from "@/api/datasets";
+import { getProjects } from "@/api/projects";
+import { DatasetForm } from "@/components/datasets/DatasetForm";
+
+interface ProjectProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ProjectDetails({ params: { id } }: ProjectProps) {
+  // const dataset = await getDataSet(id);
+  // const partners = await getDataPartners();
+  // const users = await getDataUsers();
+  // const projects = await getProjects();
+  // const permissions = await getDatasetPermissions(id);
+
+  return (
+    // <DatasetForm
+    //   dataset={dataset}
+    //   dataPartners={partners}
+    //   users={users}
+    //   projects={projects}
+    //   permissions={permissions.permissions}
+    // />
+    <div></div>
+  );
+}
