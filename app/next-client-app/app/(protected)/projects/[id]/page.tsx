140a1591263c4d5f7d475809c62958ac9ee8f942
andrewthien <anretrithien@gmail.com>
2024-10-11 11:15:22 +0100

    modify API call

--- app/next-client-app/app/(protected)/projects/[id]/page.tsx
+++ app/next-client-app/app/(protected)/projects/[id]/page.tsx
@@ -1,12 +1,9 @@
-import { getScanReports } from "@/api/scanreports";
 import { DataTable } from "@/components/data-table";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { objToQuery } from "@/lib/client-utils";
-import { ScanReportsTableFilter } from "@/components/scanreports/ScanReportsTableFilter";
 import { FilterParameters } from "@/types/filter";
-import { getproject } from "@/api/projects";
 import { columns } from "./columns";
 import { DataTableFilter } from "@/components/data-table/DataTableFilter";
+import { getDataSets } from "@/api/datasets";
 
 interface ProjectDetailProps {
   params: {
@@ -19,25 +16,22 @@ export default async function ProjectDetail({
   params: { id },
   searchParams,
 }: ProjectDetailProps) {
-  // const defaultParams = {
-  //   hidden: false,
-  //   page_size: 10,
-  //   parent_dataset: id,
-  // };
-  // const combinedParams = { ...defaultParams, ...searchParams };
-
-  // const query = objToQuery(combinedParams);
-  // const scanReports = await getScanReports(query);
-  const project = await getproject(id);
-  const datasets = project.datasets;
+  const defaultParams = {
+    hidden: false,
+    page_size: 10,
+    project: id,
+  };
+  const combinedParams = { ...defaultParams, ...searchParams };
+  const query = objToQuery(combinedParams);
+  const datasets = await getDataSets(query);
   const filter = <DataTableFilter filter="name" />;
 
   return (
     <div>
       <DataTable
         columns={columns}
-        data={datasets}
-        count={datasets.length}
+        data={datasets.results}
+        count={datasets.count}
         Filter={filter}
       />
     </div>
