229333b455be12afaf4191236f1759c3815e61a1
andrewthien <anretrithien@gmail.com>
2024-10-11 12:42:30 +0100

    update table used for datasets list in project

--- app/next-client-app/app/(protected)/projects/[id]/page.tsx
+++ app/next-client-app/app/(protected)/projects/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { DataTable } from "@/components/data-table";
 import { objToQuery } from "@/lib/client-utils";
 import { FilterParameters } from "@/types/filter";
-import { columns } from "./columns";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { DataTableFilter } from "@/components/data-table/DataTableFilter";
 import { getDataSets } from "@/api/datasets";
+import { columns } from "../../datasets/columns";
 
 interface ProjectDetailProps {
   params: {
@@ -27,13 +28,41 @@ export default async function ProjectDetail({
   const filter = <DataTableFilter filter="name" />;
 
   return (
-    <div>
-      <DataTable
-        columns={columns}
-        data={datasets.results}
-        count={datasets.count}
-        Filter={filter}
-      />
-    </div>
+    <Tabs
+      defaultValue={
+        (searchParams as any)?.hidden
+          ? (searchParams as any)?.hidden === "true"
+            ? "archived"
+            : "active"
+          : "active"
+      }
+    >
+      <div className="flex justify-between items-center">
+        <TabsList>
+          <a href="?hidden=false" className="h-full">
+            <TabsTrigger value="active">Active Datasets</TabsTrigger>
+          </a>
+          <a href="?hidden=true" className="h-full">
+            <TabsTrigger value="archived">Archived Datasets</TabsTrigger>
+          </a>
+        </TabsList>
+      </div>
+      <TabsContent value="active">
+        <DataTable
+          columns={columns}
+          data={datasets.results}
+          count={datasets.count}
+          Filter={filter}
+        />
+      </TabsContent>
+      <TabsContent value="archived">
+        <DataTable
+          columns={columns}
+          data={datasets.results}
+          count={datasets.count}
+          Filter={filter}
+        />
+      </TabsContent>
+    </Tabs>
   );
 }
