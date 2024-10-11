bc95a880c57b912eaae7b3473e524eb61b332454
andrewthien <anretrithien@gmail.com>
2024-10-11 12:42:47 +0100

    modify table for project index

--- app/next-client-app/app/(protected)/projects/columns.tsx
+++ app/next-client-app/app/(protected)/projects/columns.tsx
@@ -6,14 +6,6 @@ import { format } from "date-fns/format";
 
 export const columns: ColumnDef<Project>[] = [
   {
-    accessorKey: "id",
-    header: ({ column }) => (
-      <DataTableColumnHeader column={column} title="ID" sortName="id" />
-    ),
-    enableHiding: false,
-    enableSorting: true,
-  },
-  {
     accessorKey: "name",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Name" sortName="name" />
@@ -35,23 +27,6 @@ export const columns: ColumnDef<Project>[] = [
     enableSorting: true,
     cell: ({ row }) => {
       const date = new Date(row.original.created_at);
-      return format(date, "MMM dd, yyyy h:mm a");
-    },
-  },
-  {
-    id: "Updated At",
-    accessorKey: "updated_at",
-    header: ({ column }) => (
-      <DataTableColumnHeader
-        column={column}
-        title="Updated At"
-        sortName="updated_at"
-      />
-    ),
-    enableHiding: true,
-    enableSorting: true,
-    cell: ({ row }) => {
-      const date = new Date(row.original.updated_at);
       return format(date, "MMM dd, yyyy h:mm a");
     },
   },
