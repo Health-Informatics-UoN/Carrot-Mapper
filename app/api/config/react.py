54108df5d0c577590a3c0f7230adac6974fead34
andrewthien <anretrithien@gmail.com>
2024-10-15 13:49:28 +0100

    cleaning

--- app/api/config/react.py
+++ app/api/config/react.py
@@ -1,9 +1,2 @@
-from shared.mapping.models import UploadStatus
-
-
 def react(request):
-    return {
-        "upload_status": [
-            {"id": id, "label": label} for id, label in UploadStatus.choices
-        ],
-    }
+    return {}
