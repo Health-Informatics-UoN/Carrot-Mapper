e23cb7acdf441cdeabbbf3dcdf64651b60bbc7f4
andrewthien <anretrithien@gmail.com>
2024-10-30 14:12:19 +0000

    initial add user button

--- app/next-client-app/components/core/menubar.tsx
+++ app/next-client-app/components/core/menubar.tsx
@@ -3,7 +3,9 @@ import { SidebarButton } from "./sidebar-button";
 import { sidebarItems } from "./menuItems";
 import { Sidebar } from "./sidebar";
 import { ModeToggle } from "./mode-toggle";
-import { LogOut } from "lucide-react";
+import { CircleUserRound, LogOut, Settings } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Separator } from "../ui/separator";
 
 export const MenuBar = ({ user }: { user: User | null }) => {
   return (
@@ -22,7 +24,7 @@ export const MenuBar = ({ user }: { user: User | null }) => {
         </Link>
 
         <div className="flex items-center gap-3">
-          <div className="flex">
+          <div className="flex items-center">
             {(!user ? sidebarItems.routes : sidebarItems.links).map(
               (link, idx) => (
                 <Link key={idx} href={link.href}>
@@ -33,11 +35,41 @@ export const MenuBar = ({ user }: { user: User | null }) => {
               )
             )}
             {user && (
-              <a href={"/accounts/logout/"}>
-                <SidebarButton icon={LogOut} className="w-full">
-                  {"Log out"}
-                </SidebarButton>
-              </a>
+              // <a href={"/accounts/logout/"}>
+              //   <SidebarButton icon={LogOut} className="w-full">
+              //     {"Log out"}
+              //   </SidebarButton>
+              // </a>
+              <div>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <div className="flex justify-between items-center mx-4">
+                      <CircleUserRound className="size-5" />
+                    </div>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[12.5rem] my-2 p-2">
+                    <div className="flex flex-col space-y-2 mt-2">
+                      <div className="flex gap-2">
+                        <span>Hi, {user.username}</span>
+                      </div>
+                      <Separator className="w-full" />
+                      <a
+                        href="/accounts/password_change/"
+                        className="flex items-center"
+                      >
+                        Change Password <Settings className="size-4 ml-2" />
+                      </a>
+                      <a
+                        href={"/accounts/logout/"}
+                        className="flex items-center"
+                      >
+                        Log Out
+                        <LogOut className="size-4 ml-2" />
+                      </a>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
             )}
           </div>
           <div>
