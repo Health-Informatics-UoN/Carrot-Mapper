30daddc0256294b979b54bee682387c420797559
andrewthien <anretrithien@gmail.com>
2024-12-04 16:32:23 +0000

    add more status and constants

---
+++ app/next-client-app/components/jobs/StageStatus.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { StageStatus } from "@/constants/job";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { Tooltip } from "react-tooltip";
+
+export function Status({ status }: { status: Status }) {
+  const statusInfo = StageStatus.find(
+    (option) => option.value === status.value
+  );
+
+  const Icon = statusInfo?.icon;
+
+  if (!Icon) {
+    return null;
+  }
+
+  return (
+    <a
+      data-tooltip-id="icon-tooltip"
+      data-tooltip-content={`${statusInfo?.label}`}
+      data-tooltip-place="top"
+      className="flex justify-center"
+    >
+      <Tooltip id="icon-tooltip" />
+      <Icon
+        className={cn(
+          statusInfo.color,
+          "size-4",
+          Icon === Loader2 && "animate-spin"
+        )}
+      />
+    </a>
+  );
+}
--- app/next-client-app/constants/job.ts
+++ app/next-client-app/constants/job.ts
@@ -1,21 +1,21 @@
-import { Check, Loader2, X } from "lucide-react";
+import { Check, CircleSlash, Loader2, X } from "lucide-react";
 
 export const JobStage = [
   { id: 1, value: "UPLOAD_SCAN_REPORT", display_name: "Upload Scan Report" },
   {
     id: 2,
     value: "BUILD_CONCEPTS_FROM_DICT",
-    display_name: "Build concepts from OMOP Data dictionary",
+    display_name: "Building concepts from OMOP Data dictionary",
   },
   {
     id: 3,
     value: "REUSE_CONCEPTS",
-    display_name: "Reuse concepts from other scan reports",
+    display_name: "Reusing concepts from other scan reports",
   },
   {
     id: 4,
     value: "GENERATE_RULES",
-    display_name: "Auto generate mapping rules from available concepts",
+    display_name: "Automatic generating mapping rules from available concepts",
   },
   {
     id: 5,
@@ -25,6 +25,18 @@ export const JobStage = [
 ];
 
 export const StageStatus = [
+  {
+    label: "Not Started",
+    icon: CircleSlash,
+    value: "NOT_STARTED",
+    color: "text-gray-600 dark:text-gray-600",
+  },
+  {
+    label: "Job Queued",
+    icon: CircleSlash,
+    value: "QUEUED",
+    color: "text-gray-600 dark:text-gray-600",
+  },
   {
     label: "Job Complete",
     icon: Check,
@@ -39,6 +51,34 @@ export const StageStatus = [
   },
   {
     label: "Job In Progress",
+    icon: Loader2,
+    value: "IN_PROGRESS",
+    color: "text-orange-500 dark:text-orange-500",
+  },
+];
+
+export const GeneralStatus = [
+  {
+    label:
+      "Not Started or No data yet. Start by update table's Person ID and Date event",
+    icon: CircleSlash,
+    value: "NOT_STARTED",
+    color: "text-gray-600 dark:text-gray-600",
+  },
+  {
+    label: "Complete. Click for details.",
+    icon: Check,
+    value: "COMPLETE",
+    color: "text-green-600 dark:text-green-600",
+  },
+  {
+    label: "Failed. Click for details.",
+    icon: X,
+    value: "FAILED",
+    color: "text-red-500 dark:text-red-500",
+  },
+  {
+    label: "In Progress. Click for details.",
     icon: Loader2,
     value: "IN_PROGRESS",
     color: "text-orange-500 dark:text-orange-500",
